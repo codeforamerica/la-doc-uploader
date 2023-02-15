3c63e7e3ac768ebfff71dcd7fe6a1f948ea232e8
Sree <72890349+sree-cfa@users.noreply.github.com>
2023-02-15 11:02:09 -0800

    Updating journey test (#19)
    Co-authored-by: Lauren Kemperman <lkemperman@codeforamerica.org>

--- src/test/java/org/ladocuploader/app/journeys/LaDocUploadFlowJourneyTest.java
+++ src/test/java/org/ladocuploader/app/journeys/LaDocUploadFlowJourneyTest.java
@@ -1,6 +1,8 @@
 package org.ladocuploader.app.journeys;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.ladocuploader.app.utils.AbstractBasePageTest;
 import org.junit.jupiter.api.Test;
@@ -8,12 +10,34 @@ import org.junit.jupiter.api.Test;
 public class LaDocUploadFlowJourneyTest extends AbstractBasePageTest {
 
   @Test
-  void fullUbiFlow() {
+  void fullUploadDocumentFlow() {
     // Landing screen
     assertThat(testPage.getTitle()).isEqualTo("Louisiana Document Uploader");
     testPage.clickButton("Upload documents");
 
     // Client Info
     assertThat(testPage.getTitle()).isEqualTo("Match Info");
+    testPage.enter("firstName", "Britney");
+    testPage.enter("lastName", "Spears");
+    testPage.clickContinue();
+
+    // How to add docs
+    assertThat(testPage.getTitle()).isEqualTo("How to add documents");
+    testPage.clickContinue();
+
+    // Upload documents
+    assertThat(testPage.getTitle()).isEqualTo("Upload documents");
+    assertThat(testPage.findElementById("form-submit-button").getAttribute("class").contains("disabled")).isTrue();
+    uploadJpgFile();
+    assertFalse(testPage.findElementById("form-submit-button").getAttribute("class").contains("disabled"));
+    testPage.clickButton("I'm finished uploading");
+
+    // Confirm submit
+    takeSnapShot("snapshot.png");
+    assertThat(testPage.getTitle()).isEqualTo("Doc submit confirmation");
+    testPage.clickButton("Yes, submit and finish");
+
+    // Confirmation page
+    assertThat(testPage.getTitle()).isEqualTo("documents sent");
   }
 }
