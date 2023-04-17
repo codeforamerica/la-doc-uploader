ead2964e8339a82b30a2ba29eafa50429b7bb1a1
Lauren Kemperman <lkemperman@codeforamerica.org>
2023-04-17 09:52:28 -0500

    create new session after invalidating in getIndex

--- SessionFilter.java
+++ SessionFilter.java
@@ -9,6 +9,5 @@ import java.io.IOException;
 public class SessionFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-
     }
 }
--- src/main/java/org/ladocuploader/app/StaticPageController.java
+++ src/main/java/org/ladocuploader/app/StaticPageController.java
@@ -1,10 +1,12 @@
 package org.ladocuploader.app;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * A controller to render static pages that are not in any flow.
@@ -19,10 +21,16 @@ public class StaticPageController {
    * @return the static page template
    */
   @GetMapping("/")
-  ModelAndView getIndex(HttpSession httpSession,
+  ModelAndView getIndex(HttpServletRequest request,
+                        HttpServletResponse response,
                         @RequestParam(required = false) String ref_id) {
     // For dev, reset session if you visit home
+    HttpSession httpSession = request.getSession();
     httpSession.invalidate();
+
+    httpSession = request.getSession(true);
+
+    // start new session
     httpSession.setAttribute("ref_id", ref_id);
 
     return new ModelAndView("index");
