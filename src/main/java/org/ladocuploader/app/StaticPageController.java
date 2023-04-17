713b6fed81bdc93672fca7613e3cc8afe6c06bd0
Lauren Kemperman <lkemperman@codeforamerica.org>
2023-04-17 08:37:41 -0500

    add back httpSession.invalidate

--- src/main/java/org/ladocuploader/app/StaticPageController.java
+++ src/main/java/org/ladocuploader/app/StaticPageController.java
@@ -6,9 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * A controller to render static pages that are not in any flow.
  */
@@ -25,7 +22,7 @@ public class StaticPageController {
   ModelAndView getIndex(HttpSession httpSession,
                         @RequestParam(required = false) String ref_id) {
     // For dev, reset session if you visit home
-//    httpSession.invalidate();
+    httpSession.invalidate();
     httpSession.setAttribute("ref_id", ref_id);
 
     return new ModelAndView("index");
