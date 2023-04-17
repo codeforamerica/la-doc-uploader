402225d4d41359f275a3412eee4c2b4e1d4a699a
Lauren Kemperman <117678574+lkemperman-cfa@users.noreply.github.com>
2023-04-17 18:21:10 -0500

    Add url params to mixpanel #184720655 (#83)

--- src/main/java/org/ladocuploader/app/StaticPageController.java
+++ src/main/java/org/ladocuploader/app/StaticPageController.java
@@ -1,9 +1,12 @@
 package org.ladocuploader.app;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * A controller to render static pages that are not in any flow.
@@ -14,13 +17,21 @@ public class StaticPageController {
   /**
    * Renders the website index page.
    *
-   * @param httpSession The current HTTP session, not null
+   * @param request The current request, not null
+   * @param response The current request response
    * @return the static page template
    */
   @GetMapping("/")
-  ModelAndView getIndex(HttpSession httpSession) {
+  ModelAndView getIndex(HttpServletRequest request,
+                        HttpServletResponse response,
+                        @RequestParam(required = false) String ref_id) {
     // For dev, reset session if you visit home
+    HttpSession httpSession = request.getSession();
     httpSession.invalidate();
+    httpSession = request.getSession(true);
+
+    // start new session
+    httpSession.setAttribute("ref_id", ref_id);
 
     return new ModelAndView("index");
   }
