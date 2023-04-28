821d5446b33d6b1ad39f19f72d648ef9425ea4aa
Sree P <sprasad@codeforamerica.org>
2023-04-27 21:05:37 -0700

    fix

--- src/main/java/org/ladocuploader/app/StaticPageController.java
+++ src/main/java/org/ladocuploader/app/StaticPageController.java
@@ -1,8 +1,8 @@
 package org.ladocuploader.app;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
