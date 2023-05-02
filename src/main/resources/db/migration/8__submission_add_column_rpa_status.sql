c50db3f908e572cf7a3ec70ab9d17031eacd85b8
Lauren Kemperman <lkemperman@codeforamerica.org>
2023-05-02 15:33:53 -0400

    modify rpa status options

--- src/main/resources/db/migration/8__submission_add_column_rpa_status.sql
+++ src/main/resources/db/migration/8__submission_add_column_rpa_status.sql
@@ -1,2 +1,2 @@
-CREATE TYPE rpa_status_type AS ENUM('in_progress', 'failed', 'succeeded');
+CREATE TYPE rpa_status_type AS ENUM('NotReady', 'Ready', 'InProgress', 'Failed', 'Complete');
 ALTER TABLE submissions ADD COLUMN rpa_status rpa_status_type;
