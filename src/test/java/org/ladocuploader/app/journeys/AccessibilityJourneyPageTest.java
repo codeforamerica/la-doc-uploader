3ca963106d1f2ae3324e874bc94db8913104f020
Lauren Kemperman <lkemperman@codeforamerica.org>
2023-05-08 10:09:37 -0400

    remove dead code from accessibility journey test + add test annotation processor for logging

--- src/test/java/org/ladocuploader/app/journeys/AccessibilityJourneyPageTest.java
+++ src/test/java/org/ladocuploader/app/journeys/AccessibilityJourneyPageTest.java
@@ -6,22 +6,13 @@ import com.deque.html.axecore.selenium.AxeBuilder;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.*;
 import org.ladocuploader.app.utils.AbstractBasePageTest;
-import org.openqa.selenium.By;
 import pages.AccessibilityTestPage;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.awaitility.Awaitility.await;
-//import static org.codeforamerica.shiba.pages.YesNoAnswer.NO;
-//import static org.codeforamerica.shiba.pages.YesNoAnswer.YES;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
 
 @Slf4j
 @Tag("a11y")
@@ -48,58 +39,6 @@ public class AccessibilityJourneyPageTest extends AbstractBasePageTest {
     static void tearDownAll() {
         generateAccessibilityReport(results);
     }
-
-//    @Test
-//    void laterDocsFlow() {
-//        when(featureFlagConfiguration.get("county-hennepin")).thenReturn(FeatureFlag.ON);
-//        when(featureFlagConfiguration.get("county-morrison")).thenReturn(FeatureFlag.OFF);
-//        when(featureFlagConfiguration.get("submit-via-api")).thenReturn(FeatureFlag.ON);
-//
-//
-//        // WHEN V2 IS ENABLED IT...
-//        // should give the option to enter zip instead of county
-//        when(featureFlagConfiguration.get("later-docs-v2-feature")).thenReturn(FeatureFlag.ON);
-//
-//        testPage.clickButton("Upload documents");
-//
-//        testPage.clickLink("Enter my zip code instead.");
-//
-//        // should direct me to email the county if my zipcode is unrecognized or unsupported
-//        testPage.enter("zipCode", "11111");
-//        testPage.clickContinue();
-//
-//        // should allow me to proceed with the flow if I enter a zip code for an active county
-//        testPage.clickLink("< Go Back");
-//        testPage.enter("zipCode", "55444");
-//        testPage.clickContinue();
-//
-//
-//        // WHEN V2 IS DISABLED IT...
-//        // should not allow me to enter my zip code
-//        when(featureFlagConfiguration.get("later-docs-v2-feature")).thenReturn(FeatureFlag.OFF);
-//        navigateTo("identifyCounty");
-//
-//        // should direct me to email docs to my county if my county is not supported
-//        testPage.enter("county", "Morrison");
-//        testPage.clickContinue();
-//
-//        // should allow me to enter personal info and continue the flow if my county is supported
-//        testPage.clickLink("< Go Back");
-//        testPage.enter("county", "Hennepin");
-//        testPage.clickContinue();
-//
-//        testPage.enter("firstName", "defaultFirstName");
-//        testPage.enter("lastName", "defaultLastName");
-//        testPage.enter("dateOfBirth", "01/12/1928");
-//        testPage.enter("ssn", "123456789");
-//        testPage.enter("caseNumber", "1234567");
-//        testPage.clickContinue();
-//
-//        // should allow me to upload documents and those documents should be sent to the ESB
-//        uploadPdfFile();
-//        await().until(uploadCompletes());
-//        testPage.clickButton("I'm finished uploading");
-//    }
 
     @Test
     void userCanCompleteTheNonExpeditedHouseholdFlow() {
