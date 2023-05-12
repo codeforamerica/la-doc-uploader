2944c3a496983239ee2ad333a8703724dbcd1a36
Lauren Kemperman <lkemperman@codeforamerica.org>
2023-05-12 13:32:35 -0400

    remove unused arguments

--- src/main/resources/static/assets/js/dropzone.js
+++ src/main/resources/static/assets/js/dropzone.js
@@ -3823,7 +3823,7 @@ function __guard__(value, transform) {
 
 function __guardMethod__(obj, methodName, transform) {
   if (typeof obj !== 'undefined' && obj !== null && typeof obj[methodName] === 'function') {
-    return transform(obj, methodName);
+    return transform(obj);
   } else {
     return undefined;
   }
