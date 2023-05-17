2ef4c6ea1cb37ef57a7aecf8b6a483efab990fd9
Sree <72890349+sree-cfa@users.noreply.github.com>
2023-05-17 06:28:14 -0700

    Fix Tracking (#159)
    * Adding missing method
    
    #185193148

--- src/main/java/org/ladocuploader/app/submission/actions/TrackClientInfo.java
+++ src/main/java/org/ladocuploader/app/submission/actions/TrackClientInfo.java
@@ -2,9 +2,11 @@ package org.ladocuploader.app.submission.actions;
 
 import formflow.library.config.submission.Action;
 import formflow.library.data.FormSubmission;
+import formflow.library.data.Submission;
+import org.ladocuploader.app.submission.MixpanelTracker;
+
 import java.util.HashMap;
 import java.util.Map;
-import org.ladocuploader.app.submission.MixpanelTracker;
 
 public abstract class TrackClientInfo implements Action {
 
@@ -15,16 +17,27 @@ public abstract class TrackClientInfo implements Action {
   }
 
   @Override
+  public void run(Submission submission) {
+    Map<String, Object> inputData = submission.getInputData();
+    trackProperties(inputData, submission.getId().toString());
+  }
+
+  @Override
   public void run(FormSubmission formSubmission) {
-    Map<String, Object> properties = new HashMap<>();
+    Map<String, Object> inputData = formSubmission.getFormData();
+    trackProperties(inputData, (String) inputData.get("submissionId"));
+  }
+
+  private void trackProperties(Map<String, Object> inputData, String submissionId) {
     Map<String, String> formData = new HashMap<>();
-    formSubmission.getFormData().forEach((k, v) -> {
+    inputData.forEach((k, v) -> {
       String value = v.toString();
       if (!(value.length() == 0) && !value.equals("[]")) {
         formData.put(k, value);
       }
     });
 
+    Map<String, Object> properties = new HashMap<>();
     properties.put("first_name", formData.containsKey("firstName"));
     properties.put("last_name", formData.containsKey("lastName"));
     properties.put("birth_date", formData.containsKey("birthDate"));
@@ -33,7 +46,7 @@ public abstract class TrackClientInfo implements Action {
     properties.put("email", formData.containsKey("emailAddress"));
     properties.put("case_number", formData.containsKey("caseNumber"));
 
-    mixpanelTracker.trackWithProfile(formData.get("submissionId"), getEventName(), properties);
+    mixpanelTracker.trackWithProfile(submissionId, getEventName(), properties);
   }
 
   abstract String getEventName();
