ed50c2e267a2153a81417a8d264a0a696c1e1662
Sree P <sprasad@codeforamerica.org>
2023-05-18 12:27:30 -0700

    percy

--- src/test/java/org/ladocuploader/app/utils/Page.java
+++ src/test/java/org/ladocuploader/app/utils/Page.java
@@ -1,14 +1,15 @@
 package org.ladocuploader.app.utils;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import io.percy.selenium.Percy;
-import java.util.List;
-import java.util.stream.Collectors;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.springframework.boot.test.web.server.LocalServerPort;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class Page {
 
@@ -32,6 +33,7 @@ public class Page {
   }
 
   private void checkForBadMessageKeys() {
+    percy.snapshot(driver.getTitle());
     assertThat(driver.getTitle()).doesNotContain("??");
     assertThat(driver.findElement(By.xpath("/html")).getText()).doesNotContain("??");
   }
