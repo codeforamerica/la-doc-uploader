0a60590060e3d17df5b54181c19ebbc02631ceb7
Lauren Kemperman <lkemperman@codeforamerica.org>
2023-05-24 13:58:59 -0400

    add template for filter

---
+++ src/main/java/org/ladocuploader/app/filters/UrlFilter.java
@@ -0,0 +1,33 @@
+package org.ladocuploader.app.filters;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class UrlFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        // check for valid request
+        log.info(request.getRequestURI());
+
+        filterChain.doFilter(servletRequest, servletResponse);
+
+    }
+}
