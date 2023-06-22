9c4f05db3ef226413de7857128e36306a969bcec
Lauren Kemperman <lkemperman@codeforamerica.org>
2023-06-22 16:02:08 -0600

    basic auth

--- src/main/java/org/ladocuploader/app/config/SecurityConfiguration.java
+++ src/main/java/org/ladocuploader/app/config/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package org.ladocuploader.app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -10,10 +11,12 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
-
   @Bean
-  SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-    httpSecurity.formLogin().disable();
-    return httpSecurity.build();
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.csrf().disable()
+            .authorizeHttpRequests((authorize) -> {
+              authorize.anyRequest().authenticated();
+            }).httpBasic(Customizer.withDefaults());
+    return http.build();
   }
 }
