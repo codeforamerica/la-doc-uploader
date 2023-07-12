02a49c8acbe8247bf424cbd60e2c60ec0c2c90ac
Lauren Kemperman <117678574+lkemperman-cfa@users.noreply.github.com>
2023-07-12 15:32:41 -0600

    turn off basic auth for prod (#222)

--- src/main/java/org/ladocuploader/app/config/SecurityConfiguration.java
+++ src/main/java/org/ladocuploader/app/config/SecurityConfiguration.java
@@ -1,30 +1,15 @@
 package org.ladocuploader.app.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
-@Profile({"prod"})
 public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((authorize) -> {
-                    try {
-                        authorize.anyRequest().authenticated();
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
-                    }
-                }).httpBasic(Customizer.withDefaults());
+        http.httpBasic().disable();
         return http.build();
     }
 }
-
