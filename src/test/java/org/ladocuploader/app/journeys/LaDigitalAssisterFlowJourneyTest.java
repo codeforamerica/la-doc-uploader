3327bd8472e79bb498a87804a813da2e5bdfe533
Sree P <sprasad@codeforamerica.org>
2023-08-24 09:28:14 -0700

    Adding timeout notice page

---
+++ src/test/java/org/ladocuploader/app/journeys/LaDigitalAssisterFlowJourneyTest.java
@@ -0,0 +1,21 @@
+package org.ladocuploader.app.journeys;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.ladocuploader.app.utils.AbstractBasePageTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+public class LaDigitalAssisterFlowJourneyTest extends AbstractBasePageTest {
+
+  @Test
+  void fullUploadDocumentFlow() {
+    // How this works
+    testPage.navigateToFlowScreen("laDigitalAssister/howThisWorks");
+    testPage.clickContinue();
+
+    // Timeout notice
+    assertThat(testPage.getTitle()).isEqualTo("Timeout notice");
+  }
+}
