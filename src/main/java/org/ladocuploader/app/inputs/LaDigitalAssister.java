bb47381f5300195d5db51f9e6f50ba99c7e67c4a
Lauren Kemperman <117678574+lkemperman-cfa@users.noreply.github.com>
2023-08-30 14:34:31 -0600

    Expedited snap (#279)

---
+++ src/main/java/org/ladocuploader/app/inputs/LaDigitalAssister.java
@@ -0,0 +1,21 @@
+package org.ladocuploader.app.inputs;
+
+import formflow.library.data.FlowInputs;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.List;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class LaDigitalAssister extends FlowInputs {
+
+  // Language
+  private String languageRead;
+  private String languageSpeak;
+  private String needInterpreter;
+
+  // Choose programs
+  private List<String> programs;
+}
+
