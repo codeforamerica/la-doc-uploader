d4192a515fe3b42dc11c7a3344cb243ae947117d
Sree P <sprasad@codeforamerica.org>
2023-09-11 17:29:55 -0500

    Adding household signpost

---
+++ src/main/java/org/ladocuploader/app/submission/conditions/IsMultiplePersonHousehold.java
@@ -0,0 +1,15 @@
+package org.ladocuploader.app.submission.conditions;
+
+import formflow.library.config.submission.Condition;
+import formflow.library.data.Submission;
+
+public class IsMultiplePersonHousehold implements Condition {
+  @Override
+  public Boolean run(Submission submission) {
+    var inputData = submission.getInputData();
+    if (inputData.containsKey("multiplePersonHousehold")) {
+      return submission.getInputData().get("multiplePersonHousehold").equals("true");
+    }
+    return false;
+  }
+}
