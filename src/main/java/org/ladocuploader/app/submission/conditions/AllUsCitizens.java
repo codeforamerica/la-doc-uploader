f38c8206f932e75d7b635f52e435104a8b1720e4
Sree P <sprasad@codeforamerica.org>
2023-10-02 16:44:54 -0500

    Adding citizenship number screen

--- src/main/java/org/ladocuploader/app/submission/conditions/AllUsCitizens.java
+++ src/main/java/org/ladocuploader/app/submission/conditions/AllUsCitizens.java
@@ -1,17 +1,12 @@
 package org.ladocuploader.app.submission.conditions;
 
-import formflow.library.config.submission.Condition;
 import formflow.library.data.Submission;
 import org.springframework.stereotype.Component;
 
 @Component
-public class AllUsCitizens implements Condition {
+public class AllUsCitizens extends BasicCondition {
   @Override
   public Boolean run(Submission submission) {
-    var inputData = submission.getInputData();
-    if (inputData.containsKey("citizenshipInd")) {
-      return submission.getInputData().get("citizenshipInd").equals("true");
-    }
-    return false;
+    return run(submission, "citizenshipInd", "true");
   }
 }
