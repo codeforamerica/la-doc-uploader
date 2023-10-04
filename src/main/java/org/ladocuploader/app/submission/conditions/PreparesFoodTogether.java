45381d6d4a28811d2ab0e02b1c35cb661207c2bb
Sree P <sprasad@codeforamerica.org>
2023-10-04 17:28:33 -0500

    cleanup

--- src/main/java/org/ladocuploader/app/submission/conditions/PreparesFoodTogether.java
+++ src/main/java/org/ladocuploader/app/submission/conditions/PreparesFoodTogether.java
@@ -1,17 +1,12 @@
 package org.ladocuploader.app.submission.conditions;
 
-import formflow.library.config.submission.Condition;
 import formflow.library.data.Submission;
 import org.springframework.stereotype.Component;
 
 @Component
-public class PreparesFoodTogether implements Condition {
+public class PreparesFoodTogether extends BasicCondition {
   @Override
   public Boolean run(Submission submission) {
-    var inputData = submission.getInputData();
-    if (inputData.containsKey("buyPrepareMealsSeparateIndicator")) {
-      return submission.getInputData().get("buyPrepareMealsSeparateIndicator").equals("true");
-    }
-    return false;
+    return run(submission, "buyPrepareMealsSeparateIndicator", "true");
   }
 }
