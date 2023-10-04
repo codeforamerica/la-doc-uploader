3dea124d499a8bcc45620fe7c324695ee123a1bf
Sree P <sprasad@codeforamerica.org>
2023-10-04 16:19:49 -0500

    Extracting reusable condition for basic checks

--- src/main/java/org/ladocuploader/app/submission/conditions/IsMultiplePersonHousehold.java
+++ src/main/java/org/ladocuploader/app/submission/conditions/IsMultiplePersonHousehold.java
@@ -1,17 +1,13 @@
 package org.ladocuploader.app.submission.conditions;
 
-import formflow.library.config.submission.Condition;
 import formflow.library.data.Submission;
 import org.springframework.stereotype.Component;
 
 @Component
-public class IsMultiplePersonHousehold implements Condition {
+public class IsMultiplePersonHousehold extends BasicCondition {
+
   @Override
   public Boolean run(Submission submission) {
-    var inputData = submission.getInputData();
-    if (inputData.containsKey("multiplePersonHousehold")) {
-      return submission.getInputData().get("multiplePersonHousehold").equals("true");
-    }
-    return false;
+    return run(submission, "multiplePersonHousehold", "true");
   }
 }
