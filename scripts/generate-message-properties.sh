f69f149c183393ba7b3b4a566349fa1b05efe3ca
Lauren Kemperman <lkemperman@codeforamerica.org>
2023-10-06 09:51:21 -0600

    clean up and address gaps in the script to generate message properties

--- scripts/generate-message-properties.sh
+++ scripts/generate-message-properties.sh
@@ -1,16 +1,27 @@
 csvFile="${1:-"/Users/lkemperman/Desktop/digital-assister-all-designs-phase-1-multi-person-snap-only-leave-comments-here.csv"}"
+outfileFilepath="${2:-messages_temp_3.properties}"
 declare -A fieldMappings
+declare -A pageTitles
+
+preprocess_input () {
+  columnValue=$1
+  processedInput=${columnValue// /}
+  echo $processedInput | sed -e 's/  */ /g' | tr A-Z a-z
+}
+
 while IFS="," read -r id frame group layer_name figma_text
 
-# TODO: identify duplicate figma text elements and make them general
+# TODO: add field names as titles
+# TODO: handle special characters
+# TODO: make the field names generic for duplicates
 
 do
-  propertyName=${frame// /}
-  propertyName=$(echo $propertyName | sed -e 's/  */ /g' | tr A-Z a-z)
-  groupName=${group// /}
-  groupName=$(echo $groupName | sed -e 's/  */ /g;s/([^)]*)//g' | tr A-Z a-z)
+  propertyName="$(preprocess_input $frame)"
+  title=$( echo "${propertyName}.title" | tr -d '"')
+  pageTitles+=( [$title]=${frame} )
+  groupName="$(preprocess_input $group)"
   fieldName=$( echo $propertyName.$groupName | tr -d '"')
-  if [ -z "$propertyName" ] || [ -z "$groupName" ] || [ -z "$figma_text" ]; then
+  if [ -z "$groupName" ] || [ -z "$figma_text" ]; then
      continue
   fi
   if [[ ${!fieldMappings[@]} =~ $fieldName ]]
@@ -19,5 +30,9 @@ do
   else
     fieldMappings+=( ["${fieldName}"]=0 )
   fi
-  echo "${fieldName}${fieldMappings[$fieldName]}=${figma_text}" | tr -d "0" | tr -d '"' >> messages_temp.properties
-done < <(tail -n +2 $csvFile)
+  echo "${fieldName}${fieldMappings[$fieldName]}=${figma_text}" | tr -d "0" | tr -d '"' >> $outfileFilepath
+done < <(sort -u -t, -k5,5 $csvFile) # this removes duplicate figma text
+# add page titles
+for title in "${!pageTitles[@]}"; do
+  echo "${title} = ${pageTitles["${title}"]}" >> $outfileFilepath
+done
