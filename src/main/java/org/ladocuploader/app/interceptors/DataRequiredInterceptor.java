4db51ce15a76b896d3a7af30be70c94efdce4f9c
Sree P <sprasad@codeforamerica.org>
2023-11-01 14:39:32 -0700

    Clean up

--- src/main/java/org/ladocuploader/app/interceptors/DataRequiredInterceptor.java
+++ src/main/java/org/ladocuploader/app/interceptors/DataRequiredInterceptor.java
@@ -35,24 +35,7 @@ public class DataRequiredInterceptor implements HandlerInterceptor {
 
   @Override
   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-
-    // submissionId --> parameter from session
-    // applicationId --> parameter from inputData
-    // Option 1: See if we're missing initial data (ex program selection) and check if submissionId is different than applicationId
-    // -> then do mitigation
-    // Option 2 (ideal): Always check if submissionId is different then applicationId
-    // -> then do mitigation
-
-
-    // Mitigation - the submissionId is different than applicationId
-    // Option 1 (ideal): Overwrite session id with previous session id (from applicationId)
-    // Option 2: Copy data associated with applicationId into new submission data - update db and update applicationId to match new session id
-    // --> Might have stray records which might affect analytics, maybe delete these?
-
-
     try {
-      checkIfSubmissionIdAndApplicantIdMatch(request);
-
       var parsedUrl = new AntPathMatcher().extractUriTemplateVariables(PATH_FORMAT, request.getRequestURI());
       var screenName = parsedUrl.get("screen");
       if (BEGINNING_PAGES.containsValue(screenName) || "GET".equals(request.getMethod())) {
@@ -62,7 +45,6 @@ public class DataRequiredInterceptor implements HandlerInterceptor {
 
       String flow = parsedUrl.get("flow");
       var requiredData = REQUIRED_DATA.get(flow);
-
 
       var session = request.getSession(false);
       if (session == null) {
@@ -112,18 +94,22 @@ public class DataRequiredInterceptor implements HandlerInterceptor {
    * Check if there's any data missing and then use the applicantId to determine next steps.
    */
   private static boolean checkForMissingData(HttpServletRequest request, HttpServletResponse response, String requiredData, UUID submissionId, Submission submission) throws IOException {
-
     var applicantIdFromRequest = request.getParameter("applicantId");
-    var applicantIdFromData = submission.getInputData().getOrDefault("applicantId", "");
-    if (applicantIdFromRequest != null && !applicantIdFromRequest.equals(applicantIdFromData)) {
-      log.warn("applicantIds do not match, param=%s inputData=%s".formatted(applicantIdFromRequest, applicantIdFromData));
+    var applicantIdFromData = (String) submission.getInputData().getOrDefault("applicantId", "");
+    if (applicantIdFromRequest != null && !applicantIdFromData.isBlank() && !applicantIdFromRequest.equals(applicantIdFromData)) {
+      log.warn("Unexpected state - applicantIds do not match, param=%s inputData=%s".formatted(applicantIdFromRequest, applicantIdFromData));
+      request.getSession().setAttribute("id", UUID.fromString(applicantIdFromData));
+      return true;
     }
 
     var parsedUrl = new AntPathMatcher().extractUriTemplateVariables(PATH_FORMAT, request.getRequestURI());
     var flowName = parsedUrl.get("flow");
     var inputName = REQUIRED_DATA.get(flowName);
-    if (submission.getInputData().getOrDefault(inputName, "").toString().isBlank()) {
+    if (submission.getInputData().get(inputName) == null) {
+      var screenName = parsedUrl.get("screen");
       log.error("Submission %s missing field data %s, redirecting".formatted(submissionId, requiredData));
+      response.sendRedirect(getRedirectUrl(screenName, flowName));
+      return false;
     }
 
     return manageSessionOrRedirect(request, response);
@@ -167,8 +153,8 @@ public class DataRequiredInterceptor implements HandlerInterceptor {
       // Middle of the flow without session or applicantId data - have to redirect
       var parsedUrl = new AntPathMatcher().extractUriTemplateVariables(PATH_FORMAT, request.getRequestURI());
       var screenName = parsedUrl.get("screen");
-      String flow = parsedUrl.get("flow");
-      String redirect_url = String.format("/flow/%s/%s?intercepted=%s", flow, BEGINNING_PAGES.get(flow), screenName);
+      var flow = parsedUrl.get("flow");
+      var redirect_url = getRedirectUrl(screenName, flow);
       log.warn("Redirecting");
       response.sendRedirect(redirect_url);
       return false;
@@ -176,9 +162,12 @@ public class DataRequiredInterceptor implements HandlerInterceptor {
       // Middle of the flow without session - but we have the applicantId to recover
       // What are the security concerns here? How can we mitigate?
       log.warn("Setting session id to %s".formatted(applicantId));
-      var session = request.getSession();
-      session.setAttribute("id", UUID.fromString(applicantId));
+      request.getSession().setAttribute("id", UUID.fromString(applicantId));
       return true;
     }
+  }
+
+  private static String getRedirectUrl(String screenName, String flow) {
+    return String.format("/flow/%s/%s?intercepted=%s", flow, BEGINNING_PAGES.get(flow), screenName);
   }
 }
