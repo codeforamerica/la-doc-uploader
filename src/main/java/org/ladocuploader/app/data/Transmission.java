4130a1e0badd3e2b8bcdce62a4c831f8193860b2
Lauren Kemperman <lkemperman@codeforamerica.org>
2023-11-28 13:19:26 -0700

    make transmission model class

---
+++ src/main/java/org/ladocuploader/app/data/Transmission.java
@@ -0,0 +1,36 @@
+package org.ladocuploader.app.data;
+
+import formflow.library.data.Submission;
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.UUID;
+
+import static javax.persistence.TemporalType.TIMESTAMP;
+
+@Entity
+@Data
+@Table(name="transmissions")
+@Component
+public class Transmission {
+
+    @Id
+    private UUID batchId;
+
+    @ManyToOne
+    @JoinColumn(name = "submission_id")
+    private Submission submission;
+
+    @Temporal(TIMESTAMP)
+    @Column(name="time_sent")
+    private Date timeSent;
+
+    @Column(name="status")
+    private String status;
+
+    @Column(name="transmission_type")
+    private String transmissionType;
+
+}
