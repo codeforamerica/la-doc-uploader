313d7ef10c7eff9a5e0f02a333c203850ea3bbf9
Ana Medrano Fernandez <amedrano@codeforamerica.org@Anas-MacBook-Pro-2.local>
2023-12-12 11:56:39 -0800

    last of action tests

--- src/test/java/org/ladocuploader/app/submission/actions/ValidateMailingAddressTest.java
+++ src/test/java/org/ladocuploader/app/submission/actions/ValidateMailingAddressTest.java
@@ -13,20 +13,20 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest()
 @ActiveProfiles("test")
-class ValidateHomeAddressTest {
+class ValidateMailingAddressTest {
     private final String BLANK_VALUE="";
     private final String NOT_BLANK_VALUE = "test";
     private final String INVALID_ZIPCODE = "54";
     private final String VALID_ZIPCODE = "12345";
 
-    private final String NO_HOME_ADDRESS_INPUT_NAME = "noHomeAddress";
-    private final String ADDRESS_1_INPUT_NAME = "homeAddressStreetAddress1";
-    private final String CITY_INPUT_NAME = "homeAddressCity";
-    private final String STATE_INPUT_NAME = "homeAddressState";
-    private final String ZIP_INPUT_NAME = "homeAddressZipCode";
+    private final String SAME_AS_HOME_ADDRESS_INPUT_NAME = "sameAsHomeAddress";
+    private final String ADDRESS_1_INPUT_NAME = "mailingAddressStreetAddress1";
+    private final String CITY_INPUT_NAME = "mailingAddressCity";
+    private final String STATE_INPUT_NAME = "mailingAddressState";
+    private final String ZIP_INPUT_NAME = "mailingAddressZipCode";
 
     @Autowired
-    private ValidateHomeAddress validator;
+    private ValidateMailingAddress validator;
 
     @Test
     public void testAddressHappyPath() {
@@ -38,7 +38,7 @@ class ValidateHomeAddressTest {
         ));
 
         Map<String, List<String>> result = validator.runValidation(form, null);
-        assertThat(result.get(NO_HOME_ADDRESS_INPUT_NAME)).isNullOrEmpty();
+        assertThat(result.get(SAME_AS_HOME_ADDRESS_INPUT_NAME)).isNullOrEmpty();
         assertThat(result.get(ADDRESS_1_INPUT_NAME)).isNullOrEmpty();
         assertThat(result.get(CITY_INPUT_NAME)).isNullOrEmpty();
         assertThat(result.get(STATE_INPUT_NAME)).isNullOrEmpty();
@@ -54,46 +54,33 @@ class ValidateHomeAddressTest {
         ));
 
         Map<String, List<String>> result = validator.runValidation(form, null);
-        assertThat(result.get(NO_HOME_ADDRESS_INPUT_NAME)).containsAll(List.of("Make sure to provide a home address or select 'I don't have a permanent address'."));
-        assertThat(result.get(ADDRESS_1_INPUT_NAME)).isNullOrEmpty();
-        assertThat(result.get(CITY_INPUT_NAME)).isNullOrEmpty();
-        assertThat(result.get(STATE_INPUT_NAME)).isNullOrEmpty();
-        assertThat(result.get(ZIP_INPUT_NAME)).isNullOrEmpty();
-    }
-
-    @Test
-    public void testAddressBlankDoesNotRaiseErrorWhenUnhoused() {
-        FormSubmission form = new FormSubmission(Map.of(
-            "noHomeAddress[]", new ArrayList<>(List.of("true")),
-            ADDRESS_1_INPUT_NAME, BLANK_VALUE,
-            CITY_INPUT_NAME, BLANK_VALUE,
-            STATE_INPUT_NAME, BLANK_VALUE,
-            ZIP_INPUT_NAME, BLANK_VALUE
-        ));
-
-        Map<String, List<String>> result = validator.runValidation(form, null);
-        assertThat(result.get(NO_HOME_ADDRESS_INPUT_NAME)).isNullOrEmpty();
-        assertThat(result.get(ADDRESS_1_INPUT_NAME)).isNullOrEmpty();
-        assertThat(result.get(CITY_INPUT_NAME)).isNullOrEmpty();
-        assertThat(result.get(STATE_INPUT_NAME)).isNullOrEmpty();
-        assertThat(result.get(ZIP_INPUT_NAME)).isNullOrEmpty();
-    }
-
-    @Test
-    public void testAddressWithMissingValuesRaiseError() {
-        FormSubmission form = new FormSubmission(Map.of(
-            ADDRESS_1_INPUT_NAME, BLANK_VALUE,
-            CITY_INPUT_NAME, BLANK_VALUE,
-            STATE_INPUT_NAME, BLANK_VALUE,
-            ZIP_INPUT_NAME, INVALID_ZIPCODE
-        ));
-
-        Map<String, List<String>> result = validator.runValidation(form, null);
-        assertThat(result.get(NO_HOME_ADDRESS_INPUT_NAME)).isNullOrEmpty();
+        assertThat(result.get(SAME_AS_HOME_ADDRESS_INPUT_NAME)).isNullOrEmpty();
         assertThat(result.get(ADDRESS_1_INPUT_NAME)).containsAll(List.of("Make sure you answer this question."));
         assertThat(result.get(CITY_INPUT_NAME)).containsAll(List.of("Make sure you answer this question."));
         assertThat(result.get(STATE_INPUT_NAME)).containsAll(List.of("Make sure you answer this question."));
         assertThat(result.get(ZIP_INPUT_NAME)).containsAll(List.of("Make sure to enter a zip code with 5 digits."));
-
     }
+
+    @Test
+    public void testHomeAddressIsCheckedWhenSameAsHome() {
+        FormSubmission form = new FormSubmission(Map.of(
+            "sameAsHomeAddress[]", new ArrayList<>(List.of("true")),
+            ADDRESS_1_INPUT_NAME, BLANK_VALUE,
+            CITY_INPUT_NAME, BLANK_VALUE,
+            STATE_INPUT_NAME, BLANK_VALUE,
+            ZIP_INPUT_NAME, BLANK_VALUE,
+            "homeAddressStreetAddress1", NOT_BLANK_VALUE,
+            "homeAddressCity", NOT_BLANK_VALUE,
+            "homeAddressState", NOT_BLANK_VALUE,
+            "homeAddressZipCode", VALID_ZIPCODE
+        ));
+
+        Map<String, List<String>> result = validator.runValidation(form, null);
+        assertThat(result.get(SAME_AS_HOME_ADDRESS_INPUT_NAME)).isNullOrEmpty();
+        assertThat(result.get(ADDRESS_1_INPUT_NAME)).isNullOrEmpty();
+        assertThat(result.get(CITY_INPUT_NAME)).isNullOrEmpty();
+        assertThat(result.get(STATE_INPUT_NAME)).isNullOrEmpty();
+        assertThat(result.get(ZIP_INPUT_NAME)).isNullOrEmpty();
+    }
+
 }
