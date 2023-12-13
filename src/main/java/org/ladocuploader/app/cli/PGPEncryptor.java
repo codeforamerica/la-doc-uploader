a919f5b8fddd71671b1c53258c11acfe4b21ee51
Sree P <sprasad@codeforamerica.org>
2023-12-13 08:56:24 -0800

    Updating encryptor to sign and encrypt at the same time instead of separately

--- src/main/java/org/ladocuploader/app/cli/PGPEncryptor.java
+++ src/main/java/org/ladocuploader/app/cli/PGPEncryptor.java
@@ -1,23 +1,15 @@
 package org.ladocuploader.app.cli;
 
-import org.bouncycastle.bcpg.BCPGOutputStream;
+import org.bouncycastle.bcpg.CompressionAlgorithmTags;
 import org.bouncycastle.bcpg.SymmetricKeyAlgorithmTags;
 import org.bouncycastle.openpgp.*;
 import org.bouncycastle.openpgp.jcajce.JcaPGPPublicKeyRingCollection;
 import org.bouncycastle.openpgp.operator.KeyFingerPrintCalculator;
-import org.bouncycastle.openpgp.operator.jcajce.JcaKeyFingerprintCalculator;
-import org.bouncycastle.openpgp.operator.jcajce.JcaPGPContentSignerBuilder;
-import org.bouncycastle.openpgp.operator.jcajce.JcePBESecretKeyDecryptorBuilder;
-import org.bouncycastle.openpgp.operator.jcajce.JcePGPDataEncryptorBuilder;
-import org.bouncycastle.openpgp.operator.jcajce.JcePublicKeyKeyEncryptionMethodGenerator;
+import org.bouncycastle.openpgp.operator.jcajce.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.io.ByteArrayOutputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.security.SecureRandom;
 import java.util.Date;
 import java.util.Iterator;
@@ -35,88 +27,21 @@ public class PGPEncryptor {
     this.sigkeyPassword = sigkeyPassword;
   }
 
-  // Move to its own class
   public byte[] signAndEncryptPayload(String filename) throws IOException {
     FileInputStream instream = new FileInputStream(filename);
-    byte[] data = instream.readAllBytes();
-    instream.close();
 
-    PGPSecretKey signingKey = getPrivateKey();
-    byte[] signedMessage = signMessage(signingKey, data);
-
+    PGPSecretKey signingKey = getSecretKey();
     PGPPublicKey pubKey = getPublicKey();
-    return encodeData(signedMessage, pubKey);
-  }
 
-  private static byte[] encodeData(byte[] data, PGPPublicKey pubKey) throws IOException {
-    PGPEncryptedDataGenerator encGen = new PGPEncryptedDataGenerator(
-        new JcePGPDataEncryptorBuilder(SymmetricKeyAlgorithmTags.AES_256)
-            .setWithIntegrityPacket(true)
-            .setSecureRandom(new SecureRandom()));
-    encGen.addMethod(new JcePublicKeyKeyEncryptionMethodGenerator(pubKey));
-
-    ByteArrayOutputStream encOut = new ByteArrayOutputStream();
-    OutputStream cOut;
+    ByteArrayOutputStream outstream = new ByteArrayOutputStream();
     try {
-      cOut = encGen.open(encOut, new byte[4096]);
+      return signAndEncryptPayload(instream, signingKey, pubKey, outstream);
     } catch (PGPException e) {
-      throw new IllegalStateException("Error creating data generator", e);
+      throw new RuntimeException(e);
+    } finally {
+      instream.close();
+      outstream.close();
     }
-    PGPLiteralDataGenerator lData = new PGPLiteralDataGenerator();
-    OutputStream pOut = lData.open(cOut, PGPLiteralData.BINARY, PGPLiteralData.CONSOLE, data.length, new Date());
-    pOut.write(data);
-
-    pOut.close();
-    cOut.close();
-
-    byte[] result = encOut.toByteArray();
-    encOut.close();
-    return result;
-  }
-
-  private byte[] signMessage(PGPSecretKey pgpSec, byte[] data) throws IOException {
-    PGPSignatureGenerator sGen;
-    try {
-      PGPPrivateKey pgpPrivKey = pgpSec.extractPrivateKey(
-          new JcePBESecretKeyDecryptorBuilder().build(sigkeyPassword.toCharArray()));
-      sGen = new PGPSignatureGenerator(
-          new JcaPGPContentSignerBuilder(pgpSec.getPublicKey().getAlgorithm(), PGPUtil.SHA1));
-
-      sGen.init(PGPSignature.BINARY_DOCUMENT, pgpPrivKey);
-    } catch (PGPException e) {
-      throw new IllegalStateException("Error creating signing generator", e);
-    }
-
-    Iterator<String> it = pgpSec.getPublicKey().getUserIDs();
-    if (it.hasNext()) {
-      PGPSignatureSubpacketGenerator spGen = new PGPSignatureSubpacketGenerator();
-      spGen.addSignerUserID(false, it.next());
-      sGen.setHashedSubpackets(spGen.generate());
-    }
-
-    ByteArrayOutputStream encOut = new ByteArrayOutputStream();
-    try {
-      PGPCompressedDataGenerator comData = new PGPCompressedDataGenerator(PGPCompressedData.ZLIB);
-      BCPGOutputStream bOut = new BCPGOutputStream(comData.open(encOut));
-      sGen.generateOnePassVersion(false).encode(bOut);
-
-      PGPLiteralDataGenerator lGen = new PGPLiteralDataGenerator();
-      OutputStream lOut = lGen.open(bOut, PGPLiteralData.BINARY, PGPLiteralData.CONSOLE, data.length, new Date());
-      lOut.write(data);
-
-      sGen.update(data);
-
-      lOut.close();
-      lGen.close();
-
-      sGen.generate().encode(bOut);
-      comData.close();
-    } catch (PGPException e) {
-      throw new IllegalStateException("Error signing message", e);
-    }
-    byte[] result = encOut.toByteArray();
-    encOut.close();
-    return result;
   }
 
   private PGPPublicKey getPublicKey() throws IOException {
@@ -141,7 +66,7 @@ public class PGPEncryptor {
     return pubKey;
   }
 
-  private PGPSecretKey getPrivateKey() throws IOException {
+  private PGPSecretKey getSecretKey() throws IOException {
     InputStream inputStream = new FileInputStream(sigkeyFilename);
     inputStream = PGPUtil.getDecoderStream(inputStream);
     PGPSecretKeyRingCollection pgpSec;
@@ -169,4 +94,59 @@ public class PGPEncryptor {
 
     throw new IllegalArgumentException("Invalid signing key");
   }
+
+  private PGPPrivateKey getPrivateKey(PGPSecretKey secretKey) throws PGPException {
+    return secretKey.extractPrivateKey(
+        new JcePBESecretKeyDecryptorBuilder().build(sigkeyPassword.toCharArray()));
+  }
+
+  private byte[] signAndEncryptPayload(InputStream inputStream, PGPSecretKey secKey, PGPPublicKey pubKey,
+                                      ByteArrayOutputStream outputStream) throws PGPException, IOException {
+    int BUFFER_SIZE = 1 << 16;
+
+    // Encryption
+    PGPEncryptedDataGenerator encGen = new PGPEncryptedDataGenerator(
+        new JcePGPDataEncryptorBuilder(SymmetricKeyAlgorithmTags.AES_256)
+            .setWithIntegrityPacket(true)
+            .setSecureRandom(new SecureRandom()));
+    encGen.addMethod(new JcePublicKeyKeyEncryptionMethodGenerator(pubKey));
+    OutputStream encOut = encGen.open(outputStream, new byte[BUFFER_SIZE]);
+
+    // Compression
+    PGPCompressedDataGenerator cGen = new PGPCompressedDataGenerator(CompressionAlgorithmTags.ZIP);
+    OutputStream cOut = cGen.open(encOut);
+
+    // Signing
+    PGPSignatureGenerator sGen = new PGPSignatureGenerator(new JcaPGPContentSignerBuilder(secKey.getPublicKey().getAlgorithm(), PGPUtil.SHA1));
+    sGen.init(PGPSignature.BINARY_DOCUMENT, getPrivateKey(secKey));
+
+    Iterator<String> it = secKey.getPublicKey().getUserIDs();
+    if (it.hasNext()) {
+      PGPSignatureSubpacketGenerator spGen = new PGPSignatureSubpacketGenerator();
+      spGen.addSignerUserID(false, it.next());
+      sGen.setHashedSubpackets(spGen.generate());
+    }
+
+    sGen.generateOnePassVersion(false).encode(cOut);
+
+    // Literal Data generator and output stream
+    byte[] data = inputStream.readAllBytes();
+    PGPLiteralDataGenerator lGen = new PGPLiteralDataGenerator();
+    OutputStream lOut = lGen.open(cOut, PGPLiteralData.BINARY, PGPLiteralData.CONSOLE, data.length, new Date());
+
+    lOut.write(data);
+    sGen.update(data);
+
+    lOut.close();
+    lGen.close();
+
+    sGen.generate().encode(cOut);
+    cOut.close();
+    cGen.close();
+    encGen.close();
+
+    return outputStream.toByteArray();
+  }
+
 }
+
