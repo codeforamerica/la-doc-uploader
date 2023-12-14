7a8f9a76837a591f7a6fed974745fb281f121288
Sree P <sprasad@codeforamerica.org>
2023-12-14 08:28:22 -0800

    Code-review, fixes and cleanup

--- src/main/java/org/ladocuploader/app/cli/SubmissionTransfer.java
+++ src/main/java/org/ladocuploader/app/cli/SubmissionTransfer.java
@@ -11,10 +11,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.ladocuploader.app.submission.StringEncryptor;
 import org.springframework.shell.standard.ShellComponent;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -50,9 +52,9 @@ public class SubmissionTransfer {
 
     // for each, add to zipfile
     List<Submission> submissionsBatch = new ArrayList<>(); // TODO Get from transmission table
-    try (FileOutputStream docMeta = new FileOutputStream(batchIndex + ".txt");
-         FileOutputStream baos = new FileOutputStream(zipFileName);
+    try (FileOutputStream baos = new FileOutputStream(zipFileName);
          ZipOutputStream zos = new ZipOutputStream(baos)) {
+      StringBuilder docMeta = new StringBuilder();
       for (Submission submission : submissionsBatch) {
         String subfolder = "1"; // TODO Something unique
         try {
@@ -66,8 +68,8 @@ public class SubmissionTransfer {
             zos.putNextEntry(entry);
             zos.write(file);
             zos.closeEntry();
-            byte[] metaEntry = generateMetaDataEntry(batchIndex, subfolder, fileName, "APP-OFS 4APP", submission);
-            docMeta.write(metaEntry);
+            String metaEntry = generateMetaDataEntry(batchIndex, subfolder, fileName, "APP-OFS 4APP", submission);
+            docMeta.append(metaEntry);
 
             log.info("Adding uploaded docs");
             List<UserFile> userFiles = fileRepositoryService.findAllBySubmission(submission);
@@ -83,39 +85,50 @@ public class SubmissionTransfer {
 
               // write doc metadata - TODO add docType
               metaEntry = generateMetaDataEntry(batchIndex, userFile.getOriginalName(), "", "", submission);
-              docMeta.write(metaEntry);
+              docMeta.append(metaEntry);
             }
           }
         } catch (Exception e) {
           log.error("Error generating file collection for submission ID {}", submission.getId(), e);
         }
-        zos.close();
-        baos.close();
-        docMeta.close();
+      } // end for loop
 
-        // Encrypt and transfer
-        byte[] data = pgpEncryptor.signAndEncryptPayload(zipFileName);
-        ftpsClient.uploadFile(zipFileName, data);
-      }
+      // Add metadata entry to zip
+      ZipEntry docEntry = new ZipEntry(batchIndex + ".txt");
+      byte[] docMetaData = docMeta.toString().getBytes();
+      docEntry.setSize(docMetaData.length);
+      zos.putNextEntry(docEntry);
+      zos.write(docMetaData);
+
+      // Encrypt and transfer
+      byte[] data = pgpEncryptor.signAndEncryptPayload(zipFileName);
+      ftpsClient.uploadFile(zipFileName, data);
     } catch (IOException ex) {
       throw new IllegalStateException(ex);
+    } finally {
+      // Cleanup
+      if (new File(zipFileName).delete()) {
+        log.info("Deleting zip file");
+      }
     }
 
     // TODO - include some stats, how many transfered, failed, successful
     log.info(String.format("Completed transfer of batch %s", batchIndex));
   }
 
-  private byte[] generateMetaDataEntry(String bactchIndex, String subfolder, String filename, String documentType, Submission submission) {
-    String metaEntry = String.format("\"%s\",", bactchIndex) +
-        String.format("\"%s\",", filename) + // TODO remove file extension
-        String.format("\"%s\",", documentType) +
-        String.format("\"%s\",", submission.getInputData().getOrDefault("firstName", "")) +
-        String.format("\"%s\",", submission.getInputData().getOrDefault("lastName", "")) +
-        String.format("\"%s\",", submission.getInputData().getOrDefault("ssns", "")) + // TODO decrypt
-        String.format("\"%s\",", submission.getInputData().getOrDefault("birthdate", "")) + // TODO format
-        String.format("\"%s\",", submission.getSubmittedAt()) + // TODO format
-        String.format("\"%s/%s/%s\",", bactchIndex, subfolder, filename);
-    return metaEntry.getBytes();
+  private String generateMetaDataEntry(String batchIndex, String subfolder, String filename, String documentType, Submission submission) {
+    Map<String, Object> inputData = submission.getInputData();
+    return "\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\"\n"
+        .formatted(
+            batchIndex,
+            filename, // TODO remove file extension
+            documentType,
+            inputData.getOrDefault("firstName", ""),
+            inputData.getOrDefault("lastName", ""),
+            inputData.getOrDefault("ssns", ""), // TODO decrypt
+            inputData.getOrDefault("birthdate", ""), // TODO format
+            submission.getSubmittedAt(), // TODO format
+            String.format("\"%s/%s/%s\",", batchIndex, subfolder, filename));
   }
 
 }
