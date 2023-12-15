d1788313a71ff2ce9831d4fcf09661297681a7d2
Lauren Kemperman <lkemperman@codeforamerica.org>
2023-12-15 08:45:12 -0700

    remove unused dynamic field

--- src/main/java/org/ladocuploader/app/inputs/LaDocUpload.java
+++ src/main/java/org/ladocuploader/app/inputs/LaDocUpload.java
@@ -1,7 +1,6 @@
 package org.ladocuploader.app.inputs;
 
 import formflow.library.data.FlowInputs;
-import formflow.library.data.annotations.DynamicField;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
