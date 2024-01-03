e76a8abc1d8dd47987f782f72414111dd8b4ddcb
Bethany Seeger <bseeger@codeforamerica.org>
2024-01-03 17:22:40 -0500

    Test out delay in static journey tests

--- src/test/java/org/ladocuploader/app/journeys/StaticPagesJourneyTest.java
+++ src/test/java/org/ladocuploader/app/journeys/StaticPagesJourneyTest.java
@@ -1,11 +1,14 @@
 package org.ladocuploader.app.journeys;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
 
+import java.util.concurrent.TimeUnit;
 import org.ladocuploader.app.utils.AbstractBasePageTest;
 import org.ladocuploader.app.utils.Page;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
 @Tag("staticPagesJourney")
 public class StaticPagesJourneyTest extends AbstractBasePageTest {
@@ -28,6 +31,9 @@ public class StaticPagesJourneyTest extends AbstractBasePageTest {
     testPage.clickLink("Privacy Policy");
     assertThat(driver.getWindowHandles().size()).isEqualTo(2);
     switchAwayFromOriginalWindow(originalWindow);
+    await().atMost(5, TimeUnit.SECONDS).until(
+        () -> testPage.getTitle().equals("Privacy Policy")
+    );
     assertThat(testPage.getTitle()).isEqualTo("Privacy Policy");
   }
 
