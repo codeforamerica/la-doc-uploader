4caf27e6126ea492268eda23cc509e1d414d9699
Lauren Kemperman <lkemperman@codeforamerica.org>
2024-01-03 15:21:00 -0700

    add condition for doc upload expiry

---
+++ src/main/java/org/ladocuploader/app/submission/conditions/IsDocUploadExpired.java
@@ -0,0 +1,31 @@
+package org.ladocuploader.app.submission.conditions;
+
+import formflow.library.config.submission.Condition;
+import formflow.library.data.Submission;
+import org.springframework.stereotype.Component;
+
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Component
+public class IsDocUploadExpired implements Condition {
+
+    public static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("MMMM d, yyyy");
+
+    public Long expiryHours = 2L;
+
+
+    @Override
+    public Boolean run(Submission submission){
+        OffsetDateTime submittedAt = submission.getSubmittedAt();
+        OffsetDateTime now = OffsetDateTime.now();
+
+        if (submittedAt != null){
+            String formattedSubmittedAt = submittedAt.format(DATE_FORMAT);
+            OffsetDateTime expiryTime = submittedAt.minusHours(expiryHours);
+
+            return expiryTime.isBefore(now);
+        }
+        return false;
+    }
+}
