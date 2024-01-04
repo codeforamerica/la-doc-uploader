39a9dd7ca9dedf102b1a0f9b71a11a0f11369fe2
Lauren Kemperman <lkemperman@codeforamerica.org>
2024-01-04 08:05:33 -0700

    move to submission utilities so that we could check in the template if doc upload is expired

--- src/main/java/org/ladocuploader/app/submission/conditions/IsDocUploadExpired.java
+++ src/main/java/org/ladocuploader/app/submission/conditions/IsDocUploadExpired.java
@@ -2,26 +2,15 @@ package org.ladocuploader.app.submission.conditions;
 
 import formflow.library.config.submission.Condition;
 import formflow.library.data.Submission;
+import org.ladocuploader.app.utils.SubmissionUtilities;
 import org.springframework.stereotype.Component;
-
-import java.time.OffsetDateTime;
 
 @Component
 public class IsDocUploadExpired implements Condition {
 
-    public Long expiryHours = 2L;
-
 
     @Override
     public Boolean run(Submission submission){
-        OffsetDateTime submittedAt = submission.getSubmittedAt();
-        OffsetDateTime now = OffsetDateTime.now();
-
-        if (submittedAt != null){
-            OffsetDateTime expiryTime = submittedAt.plusHours(expiryHours);
-
-            return expiryTime.isAfter(now);
-        }
-        return false;
+        return SubmissionUtilities.isDocUploadExpired(submission);
     }
 }
