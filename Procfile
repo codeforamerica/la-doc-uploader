3af77191aca1435dde84ea0c3bbee06a1b50961b
Sree <72890349+sree-cfa@users.noreply.github.com>
2024-01-09 11:16:09 -0800

    Adding chron job for snap application transmission (#496)

---
+++ Procfile
@@ -0,0 +1,3 @@
+cron: exec supercronic /opt/form-flow-starter-app/crontab
+cmd: java -jar /opt/form-flow-starter-app/app.jar
+web: java -Dserver.port=$PORT $JAVA_OPTS -jar build/libs/*.jar
--- src/test/java/org/ladocuploader/app/cli/SubmissionTransferTest.java
+++ src/test/java/org/ladocuploader/app/cli/SubmissionTransferTest.java
@@ -28,9 +28,9 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasItem;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
@@ -60,11 +60,12 @@ class SubmissionTransferTest {
   @MockBean
   FtpsClient ftpsClient;
 
-  Submission submission;
+  private Submission submissionWithDocs;
+  private Submission submissionWithoutDocs;
+  private Submission invalidSubmission;
 
   @BeforeEach
   void setup() throws IOException {
-    when(pdfService.getFilledOutPDF(any())).thenReturn("some bytes".getBytes());
     doAnswer(args -> {
       String zipfilename = (String) args.getArguments()[0];
       String transmitlocation = "mocktransmit_" + args.getArguments()[0];
@@ -75,8 +76,106 @@ class SubmissionTransferTest {
       return null;
     }).when(ftpsClient).uploadFile(anyString(), any());
 
+    submissionWithoutDocs = queueSubmissionWithoutDocs();
+    submissionWithDocs = queueSubmissionWithDocs();
+    invalidSubmission = queueInvalidSubmission();
+
+    when(pdfService.getFilledOutPDF(eq(submissionWithoutDocs))).thenReturn("some bytes".getBytes());
+    when(pdfService.getFilledOutPDF(eq(submissionWithDocs))).thenReturn("some other bytes".getBytes());
+    when(pdfService.getFilledOutPDF(eq(invalidSubmission))).thenThrow(new IllegalArgumentException("There was an error generating the PDF"));
+  }
+
+  @Test
+  public void transmitZipFile() throws IOException {
+    submissionTransfer.transferSubmissions();
+
+    File zipFile = new File("mocktransmit_00050000000.zip");
+    assertTrue(zipFile.exists());
+
+    verify(ftpsClient).uploadFile(any(), any());
+
+    Transmission transmittedWithDocs = transmissionRepository.findBySubmissionAndTransmissionType(submissionWithDocs, TransmissionType.SNAP);
+    assertThat(transmittedWithDocs.getStatus(), equalTo(TransmissionStatus.Complete));
+    assertNull(transmittedWithDocs.getDocumentationErrors());
+
+    Transmission transmittedWithoutDocs = transmissionRepository.findBySubmissionAndTransmissionType(submissionWithoutDocs, TransmissionType.SNAP);
+    assertThat(transmittedWithoutDocs.getStatus(), equalTo(TransmissionStatus.Complete));
+    assertNull(transmittedWithoutDocs.getDocumentationErrors());
+
+    Transmission invalidTransmission = transmissionRepository.findBySubmissionAndTransmissionType(invalidSubmission, TransmissionType.SNAP);
+    assertThat(invalidTransmission.getStatus(), equalTo(TransmissionStatus.Failed));
+    assertThat(invalidTransmission.getDocumentationErrors().get("error"), equalTo("There was an error generating the PDF"));
+    assertThat(invalidTransmission.getDocumentationErrors().get("subfolder"), equalTo("3"));
+
+    String destDir = "output";
+    List<String> fileNames = unzip(zipFile.getPath(), destDir);
+
+    assertThat(fileNames, hasItem("output/1/"));
+    assertThat(fileNames, hasItem("output/1/SNAP_application.pdf"));
+    assertThat(fileNames, hasItem("output/2/"));
+    assertThat(fileNames, hasItem("output/2/SNAP_application.pdf"));
+    assertThat(fileNames, hasItem("output/2/originalFilename.png"));
+    assertThat(fileNames, hasItem("output/2/2_originalFilename.png"));
+    assertThat(fileNames, hasItem("output/2/weird/:\\filename.jpg"));
+    assertThat(fileNames, hasItem("output/00050000000.txt"));
+    assertEquals(8, fileNames.size());
+
+    // cleanup
+    zipFile.delete();
+  }
+
+  private Submission queueInvalidSubmission() {
     Date submittedDate = new Date(new Date().getTime() - (1000 * 60 * 60) * 2);
-    submission = Submission.builder()
+    Submission submission = Submission.builder()
+        .submittedAt(submittedDate)
+        .flow("laDigitalAssister")
+        .urlParams(new HashMap<>())
+        .inputData(new HashMap<>()).build();
+    submissionRepository.save(submission);
+    saveTransmissionRecord(submission);
+    return submission;
+  }
+
+  private Submission queueSubmissionWithDocs() {
+    Date submittedDate = new Date(new Date().getTime() - (1000 * 60 * 60) * 2);
+    var submission = Submission.builder()
+        .submittedAt(submittedDate)
+        .flow("laDigitalAssister")
+        .urlParams(new HashMap<>())
+        .inputData(new HashMap<>(Map.of(
+            "firstName", "Other",
+            "lastName", "McOtherson",
+            "birthDay", "1",
+            "birthMonth", "11",
+            "birthYear", "1111",
+            "signature", "Other McOtherson sig"
+        ))).build();
+    submissionRepository.save(submission);
+    UUID docId1 = saveUserFile(submission, "applicant1_birth-certificate.jpeg", "originalFilename.png");
+    UUID docId2 = saveUserFile(submission, "applicant1_license.jpeg", "originalFilename.png");
+    UUID docId3 = saveUserFile(submission, "applicant1_pay-stub.png", "weird/:\\filename.jpg");
+    saveTransmissionRecord(submission);
+    submission.getInputData().putAll(Map.of("uploadDocuments", "[\"%s\",\"%s\",\"%s\"]".formatted(docId1, docId2, docId3),
+        "docType_wildcard_" + docId1, "BirthCertificate",
+        "docType_wildcard_" + docId2, "Other",
+        "docType_wildcard_" + docId3, "DriversLicense"));
+    submissionRepository.save(submission);
+
+    Stream.of("applicant1_birth-certificate.jpeg", "applicant1_license.jpeg", "applicant1_pay-stub.png")
+        .forEach(filename -> {
+          try (FileInputStream docFileStream = new FileInputStream("src/test/resources/inputs/" + filename)) {
+            byte[] docFileData = docFileStream.readAllBytes();
+            when(fileRepository.get(eq(filename))).thenReturn(new CloudFile(10L, docFileData));
+          } catch (IOException e) {
+            throw new IllegalStateException(e);
+          }
+        });
+    return submission;
+  }
+
+  private Submission queueSubmissionWithoutDocs() {
+    Date submittedDate = new Date(new Date().getTime() - (1000 * 60 * 60) * 2);
+    Submission submission = Submission.builder()
         .submittedAt(submittedDate)
         .flow("laDigitalAssister")
         .urlParams(new HashMap<>())
@@ -90,39 +189,7 @@ class SubmissionTransferTest {
         )).build();
     submissionRepository.save(submission);
     saveTransmissionRecord(submission);
-
-    var submissionWithDocs = Submission.builder()
-        .submittedAt(submittedDate)
-        .flow("laDigitalAssister")
-        .urlParams(new HashMap<>())
-        .inputData(new HashMap<>(Map.of(
-            "firstName", "Other",
-            "lastName", "McOtherson",
-            "birthDay", "1",
-            "birthMonth", "11",
-            "birthYear", "1111",
-            "signature", "Other McOtherson sig"
-        ))).build();
-    submissionRepository.save(submissionWithDocs);
-    UUID docId1 = saveUserFile(submissionWithDocs, "applicant1_birth-certificate.jpeg", "originalFilename.png");
-    UUID docId2 = saveUserFile(submissionWithDocs, "applicant1_license.jpeg", "originalFilename.png");
-    UUID docId3 = saveUserFile(submissionWithDocs, "applicant1_pay-stub.png", "weird/:\\filename.jpg");
-    saveTransmissionRecord(submissionWithDocs);
-    submissionWithDocs.getInputData().putAll(Map.of("uploadDocuments", "[\"%s\",\"%s\",\"%s\"]".formatted(docId1, docId2, docId3),
-        "docType_wildcard_" + docId1, "BirthCertificate",
-        "docType_wildcard_" + docId2, "Other",
-        "docType_wildcard_" + docId3, "DriversLicense"));
-    submissionRepository.save(submissionWithDocs);
-
-    Stream.of("applicant1_birth-certificate.jpeg", "applicant1_license.jpeg", "applicant1_pay-stub.png")
-        .forEach(filename -> {
-          try (FileInputStream docFileStream = new FileInputStream("src/test/resources/inputs/" + filename)) {
-            byte[] docFileData = docFileStream.readAllBytes();
-            when(fileRepository.get(eq(filename))).thenReturn(new CloudFile(10L, docFileData));
-          } catch (IOException e) {
-            throw new IllegalStateException(e);
-          }
-        });
+    return submission;
   }
 
   private void saveTransmissionRecord(Submission submission) {
@@ -140,32 +207,6 @@ class SubmissionTransferTest {
     docFile.setOriginalName(originalName);
     userFileRepository.save(docFile);
     return docFile.getFileId();
-  }
-
-  @Test
-  public void transmitZipFile() throws IOException {
-    submissionTransfer.transferSubmissions();
-
-    File zipFile = new File("mocktransmit_00050000000.zip");
-    assertTrue(zipFile.exists());
-
-    verify(ftpsClient).uploadFile(any(), any());
-
-    String destDir = "output";
-    List<String> fileNames = unzip(zipFile.getPath(), destDir);
-
-    assertThat(fileNames, hasItem("output/1/"));
-    assertThat(fileNames, hasItem("output/1/SNAP_application.pdf"));
-    assertThat(fileNames, hasItem("output/2/"));
-    assertThat(fileNames, hasItem("output/2/SNAP_application.pdf"));
-    assertThat(fileNames, hasItem("output/2/originalFilename.png"));
-    assertThat(fileNames, hasItem("output/2/2_originalFilename.png"));
-    assertThat(fileNames, hasItem("output/2/weird/:\\filename.jpg"));
-    assertThat(fileNames, hasItem("output/00050000000.txt"));
-    assertEquals(8, fileNames.size());
-
-    // cleanup
-    zipFile.delete();
   }
 
   private static List<String> unzip(String zipFilePath, String destDir) {
