61633e11ac625d0586000c542ccff6f593017319
Sree P <sprasad@codeforamerica.org>
2024-01-24 16:52:46 -0800

    Logging and test changes

--- src/main/java/org/ladocuploader/app/cli/MockFtpsClientImpl.java
+++ src/main/java/org/ladocuploader/app/cli/MockFtpsClientImpl.java
@@ -4,6 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 @Slf4j
 @Component
 @Profile({"dev", "test", "demo"})
@@ -13,5 +16,12 @@ public class MockFtpsClientImpl implements FtpsClient {
   public void uploadFile(String zipFilename, byte[] data) {
     // Do nothing
     log.info("Mock uploading file " + zipFilename);
+    try {
+      FileOutputStream out = new FileOutputStream("mockTransferServer/" + zipFilename);
+      out.write(data);
+      out.close();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
