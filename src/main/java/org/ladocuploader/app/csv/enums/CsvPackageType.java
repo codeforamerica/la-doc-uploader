c05986393d5ecc42b04160e8dd33d6be425150d7
Lauren Kemperman <lkemperman@codeforamerica.org>
2024-02-01 08:38:07 -0700

    do not include documents for WIC - ECE only

--- src/main/java/org/ladocuploader/app/csv/enums/CsvPackageType.java
+++ src/main/java/org/ladocuploader/app/csv/enums/CsvPackageType.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 public enum CsvPackageType {
 <<<<<<< HEAD
+<<<<<<< HEAD
   ECE_PACKAGE (
           List.of(PARENT_GUARDIAN, STUDENT, RELATIONSHIP, ECE_APPLICATION),
           "nola-ps",
@@ -29,6 +30,10 @@ public enum CsvPackageType {
 =======
   ECE_PACKAGE (List.of(PARENT_GUARDIAN, STUDENT, RELATIONSHIP, ECE_APPLICATION), "/la-du-moveit-transfer/nola-ps"),
   WIC_PACKAGE (List.of(WIC_APPLICATION), "/la-du-moveit-transfer/dcfs");
+=======
+  ECE_PACKAGE (List.of(PARENT_GUARDIAN, STUDENT, RELATIONSHIP, ECE_APPLICATION), "/la-du-moveit-transfer/nola-ps", true),
+  WIC_PACKAGE (List.of(WIC_APPLICATION), "/la-du-moveit-transfer/dcfs", false);
+>>>>>>> 33f952b0 (do not include documents for WIC - ECE only)
 
 >>>>>>> 449ac468 (add base class + 2 subclasses extending PGPEncryptor)
   private final List<CsvType> csvTypeList;
@@ -36,6 +41,7 @@ public enum CsvPackageType {
   @Getter
   private final String uploadLocation;
 
+<<<<<<< HEAD
   @Getter
   private final Boolean includeDocumentation;
 
@@ -49,4 +55,22 @@ public enum CsvPackageType {
     this.encryptPackage = encryptPackage;
   }
 
+=======
+  private final Boolean includeDocuments;
+  CsvPackageType(List<CsvType> csvTypeList, String uploadLocation, Boolean includeDocuments) {
+    this.csvTypeList = csvTypeList;
+    this.uploadLocation = uploadLocation;
+    this.includeDocuments = includeDocuments;
+  }
+
+  public List<CsvType> getCsvTypeList() {
+    return this.csvTypeList;
+  }
+
+  public String getUploadLocation() {
+    return this.uploadLocation;
+  }
+
+  public Boolean getIncludeDocuments() {return this.includeDocuments;}
+>>>>>>> 33f952b0 (do not include documents for WIC - ECE only)
 }
