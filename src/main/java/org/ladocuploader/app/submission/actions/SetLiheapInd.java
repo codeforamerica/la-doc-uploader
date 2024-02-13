d36a6a757d8d581284ca750fab1e5dd0e03c0c30
Lauren Kemperman <lkemperman@codeforamerica.org>
2024-02-13 15:52:53 -0700

    add afterSave action

---
+++ src/main/java/org/ladocuploader/app/submission/actions/SetLiheapInd.java
@@ -0,0 +1,23 @@
+package org.ladocuploader.app.submission.actions;
+
+import formflow.library.config.submission.Action;
+import formflow.library.data.Submission;
+import org.springframework.stereotype.Component;
+
+import java.time.DayOfWeek;
+import java.time.OffsetDateTime;
+import java.util.Map;
+
+@Component
+public class SetLiheapInd implements Action {
+    @Override
+    public void run(Submission submission) {
+
+        Map<String, Object> inputData = submission.getInputData();
+        if (inputData.containsKey("receivesEnergyAssistance") &&
+                inputData.get("receivesEnergyAssistance").toString().equals("false")) {
+            submission.getInputData().put("assistanceThroughLiheap", "false");
+
+        }
+    }
+}
