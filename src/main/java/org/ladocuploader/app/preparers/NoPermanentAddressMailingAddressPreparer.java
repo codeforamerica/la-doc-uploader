f3ed1bc02b49c93a37f40a1b13e724868bbd26b7
Lauren Kemperman <lkemperman@codeforamerica.org>
2024-03-05 11:11:50 -0700

    add preparer to extract the parish details if no home address

--- src/main/java/org/ladocuploader/app/preparers/NoPermanentAddressMailingAddressPreparer.java
+++ src/main/java/org/ladocuploader/app/preparers/NoPermanentAddressMailingAddressPreparer.java
@@ -2,6 +2,7 @@ package org.ladocuploader.app.preparers;
 
 import formflow.library.data.Submission;
 import formflow.library.pdf.*;
+import org.ladocuploader.app.data.enums.Parish;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -21,53 +22,18 @@ public class NoPermanentAddressMailingAddressPreparer implements SubmissionField
 
     Map<String, Object> inputData = submission.getInputData();
 
-    Boolean noHomeAddress = inputData.getOrDefault("noHomeAddress", "false").equals("true");
-
-//      mailingAddressZipCode: mailingAddressZipCode
-//      mailingAddressState: mailingAddressState
-//      mailingAddressCity: mailingAddressCity
-//      mailingAddressStreetAddress2: mailingAddressStreetAddress2
-//      mailingAddressStreetAddress1: mailingAddressStreetAddress1
+    boolean noHomeAddress = inputData.getOrDefault("noHomeAddress[]", List.of("false")).equals(List.of("true"));
 
     if (noHomeAddress){
-
+      // get parish address and map to mailing address
+      String selectedParish = (String) submission.getInputData().get("parish");
+      Parish parishDetails = Parish.valueOf(selectedParish);
+      results.put("mailingAddressZipCode", new SingleField("mailingAddressZipCode", parishDetails.getMailingAddressZipcode(), null));
+      results.put("mailingAddressState", new SingleField("mailingAddressState", parishDetails.getMailingAddressState(), null));
+      results.put("mailingAddressCity", new SingleField("mailingAddressCity", parishDetails.getMailingAddressCity(), null));
+      results.put("mailingAddressStreetAddress1", new SingleField("mailingAddressStreetAddress1", parishDetails.getMailingAddress1(), null));
+      results.put("mailingAddressStreetAddress2", new SingleField("mailingAddressStreetAddress2", parishDetails.getMailingAddress1(), null));
     }
-
-    // Convert "who does this apply to" responses to a boolean indicator
-//    Map.of("students[]", "applicantStudentInd",
-//            "nonCitizens[]", "applicantNonCitizenInd",
-//            "homeless[]", "applicantHomelessInd")
-//        .forEach((key, value) -> {
-//          var inputs = (List<String>) inputData.getOrDefault(key, new ArrayList<>());
-//          var ind = inputs != null && inputs.contains("you") ? "Yes" : "No";
-//          results.put(value, new SingleField(value, ind, null));
-//        });
-//
-//    // Format birthday
-//    var birthday = Stream.of("birthMonth", "birthDay", "birthYear")
-//        .map(inputData::get)
-//        .reduce((e, c) -> e + "/" + c)
-//        .get();
-//    results.put("applicantBirthday", new SingleField("applicantBirthdayFormatted", (String) birthday, null));
-//
-//    var educationStatus = inputData.get("highestEducation");
-//    results.put("highestEducation", new SingleField("highestEducationFormatted", PDF_EDUCATION_MAP.get(educationStatus), null));
-//
-//    String languageRead = (String) submission.getInputData().getOrDefault("languageRead", "English");
-//    if (languageRead.equals("English")){
-//        results.put("canReadAndUnderstandEnglish", new SingleField("canReadAndUnderstandEnglish", "true", null));
-//    } else {
-//        results.put("canReadAndUnderstandEnglish", new SingleField("canReadAndUnderstandEnglish", "false", null));
-//        results.put("whatLanguageCanReadAndUnderstand", new SingleField("whatLanguageCanReadAndUnderstand", languageRead, null));
-//    }
-//
-//    // If they said they don't want us to collect this info, don't pass it on, even if they had previously set it.
-//    // This check helps if they went back and forward in the pages changing values. The data that prepares these fields is
-//    // the default OneToManyPreparer in the ffl. This code just overwrites the fields to _unset_ them.
-//    if (submission.getInputData().getOrDefault("permissionToAskAboutRace", "false").equals("false")) {
-//      results.put("raceSelected", new CheckboxField("raceSelected", List.of(), null));
-//      results.put("ethnicitySelected", new SingleField("ethnicitySelected", "", null));
-//    }
 
     return results;
   }
