adfb099191af472b3218108fcc7b747e677ed87d
Lauren Kemperman <lkemperman@codeforamerica.org>
2024-03-06 17:23:23 -0700

    fix tests and add input name

--- src/main/java/org/ladocuploader/app/inputs/LaDigitalAssister.java
+++ src/main/java/org/ladocuploader/app/inputs/LaDigitalAssister.java
@@ -354,6 +354,8 @@ public class LaDigitalAssister extends FlowInputs {
   private String adultsWorking;
   private String guardiansHaveDisabilityInd;
 
+  private String disabilityInd;
+
   // Final Screen
   private String needsNewEbtCard;
 
