e595bb7bbf541f5b11fae8e46311ae858580cda6
Lauren Kemperman <lkemperman@codeforamerica.org>
2024-03-07 08:32:50 -0700

    add state received benefits per household member state selector

--- src/main/java/org/ladocuploader/app/inputs/LaDigitalAssister.java
+++ src/main/java/org/ladocuploader/app/inputs/LaDigitalAssister.java
@@ -131,6 +131,9 @@ public class LaDigitalAssister extends FlowInputs {
   @Size(min=11, max=11, message="{error.invalid-ssn}")
   private String ssn;
 
+  @DynamicField
+  private String outOfStateBenefitsRecipient;
+
   @Size(min=11, max=11, message="{error.invalid-ssn}")
   @DynamicField
   private String householdMemberSsn;
