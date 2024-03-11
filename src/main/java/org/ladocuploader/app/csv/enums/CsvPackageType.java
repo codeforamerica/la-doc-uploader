d8c5607480ef58444855188a3048ea95d709e877
Lauren Kemperman <lkemperman@codeforamerica.org>
2024-03-11 08:46:12 -0600

    fix cron expression?

--- src/main/java/org/ladocuploader/app/csv/enums/CsvPackageType.java
+++ src/main/java/org/ladocuploader/app/csv/enums/CsvPackageType.java
@@ -11,9 +11,6 @@ import static org.ladocuploader.app.csv.enums.CsvType.WIC_APPLICATION;
 import java.util.List;
 
 public enum CsvPackageType {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
   ECE_PACKAGE (
           List.of(PARENT_GUARDIAN, STUDENT, RELATIONSHIP, ECE_APPLICATION),
           "nola-ps",
@@ -28,40 +25,11 @@ public enum CsvPackageType {
   );
 
   @Getter
-=======
-  ECE_PACKAGE (List.of(PARENT_GUARDIAN, STUDENT, RELATIONSHIP, ECE_APPLICATION), "/la-du-moveit-transfer/nola-ps"),
-  WIC_PACKAGE (List.of(WIC_APPLICATION), "/la-du-moveit-transfer/dcfs");
-=======
-  ECE_PACKAGE (List.of(PARENT_GUARDIAN, STUDENT, RELATIONSHIP, ECE_APPLICATION), "/la-du-moveit-transfer/nola-ps", true),
-  WIC_PACKAGE (List.of(WIC_APPLICATION), "/la-du-moveit-transfer/dcfs", false);
->>>>>>> 33f952b0 (do not include documents for WIC - ECE only)
-
->>>>>>> 449ac468 (add base class + 2 subclasses extending PGPEncryptor)
-=======
-  ECE_PACKAGE (
-          List.of(PARENT_GUARDIAN, STUDENT, RELATIONSHIP, ECE_APPLICATION),
-          "nola-ps",
-          true,
-          true
-  ),
-  WIC_PACKAGE (
-          List.of(WIC_APPLICATION),
-          "dcfs",
-          false,
-          true
-  );
-
-  @Getter
->>>>>>> b663d9a1 (add override for in-memory upload file in sftp impl, encrypt for packages with encryption flag)
   private final List<CsvType> csvTypeList;
 
   @Getter
   private final String uploadLocation;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b663d9a1 (add override for in-memory upload file in sftp impl, encrypt for packages with encryption flag)
   @Getter
   private final Boolean includeDocumentation;
 
@@ -69,35 +37,10 @@ public enum CsvPackageType {
   private final Boolean encryptPackage;
 
   CsvPackageType(List<CsvType> csvTypeList, String uploadLocation, Boolean includeDocumentation, Boolean encryptPackage) {
-<<<<<<< HEAD
     this.csvTypeList = csvTypeList;
     this.uploadLocation = uploadLocation;
     this.includeDocumentation = includeDocumentation;
     this.encryptPackage = encryptPackage;
   }
 
-=======
-  private final Boolean includeDocuments;
-  CsvPackageType(List<CsvType> csvTypeList, String uploadLocation, Boolean includeDocuments) {
-=======
->>>>>>> b663d9a1 (add override for in-memory upload file in sftp impl, encrypt for packages with encryption flag)
-    this.csvTypeList = csvTypeList;
-    this.uploadLocation = uploadLocation;
-    this.includeDocumentation = includeDocumentation;
-    this.encryptPackage = encryptPackage;
-  }
-
-<<<<<<< HEAD
-
-<<<<<<< HEAD
-  public String getUploadLocation() {
-    return this.uploadLocation;
-  }
-
-  public Boolean getIncludeDocuments() {return this.includeDocuments;}
->>>>>>> 33f952b0 (do not include documents for WIC - ECE only)
-=======
->>>>>>> b663d9a1 (add override for in-memory upload file in sftp impl, encrypt for packages with encryption flag)
-=======
->>>>>>> c502b506 (add separate encryptor for ECE - also waiting on key set)
 }
