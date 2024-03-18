a51299690c77b8d52cf169cd1be6f27bd3ed811d
Sree <72890349+sree-cfa@users.noreply.github.com>
2024-03-18 11:24:30 -0700

    Prod fixes (#689)
    * no longer swallowing exception on file transfer.
    * revert batchIndex for when this happens
    * update unit test to check this situation
    * other - refactoring submissionID recovery

--- src/main/java/org/ladocuploader/app/data/TransmissionRepository.java
+++ src/main/java/org/ladocuploader/app/data/TransmissionRepository.java
@@ -28,4 +28,7 @@ public interface TransmissionRepository extends JpaRepository<Transmission, UUID
 
     @Query(value = "SELECT nextval('batch_seq')")
     Long nextValueBatchSequence();
+
+    @Query(value = "SELECT setval('batch_seq', :rollbackValue)")
+    void rollbackBatchSequence(Long rollbackValue);
 }
