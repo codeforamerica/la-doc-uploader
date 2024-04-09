f8de03adf4460526aee42f39938f3ad952415da8
spokenbird <agonzalez@codeforamerica.org>
2024-04-09 15:15:58 -0700

    Add demo to MockPGPEncryptorImpl

--- src/main/java/org/ladocuploader/app/cli/MockPGPEncryptorImpl.java
+++ src/main/java/org/ladocuploader/app/cli/MockPGPEncryptorImpl.java
@@ -10,7 +10,7 @@ import java.io.InputStream;
 
 @Slf4j
 @Component
-@Profile({"dev", "test"})
+@Profile({"dev", "test", "demo"})
 public class MockPGPEncryptorImpl implements PGPEncryptor {
 
   @Override
