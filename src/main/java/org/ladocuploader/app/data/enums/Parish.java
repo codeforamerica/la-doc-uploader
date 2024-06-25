0701366f52965ee947f3ab8f19e8d412a8fe7368
spokenbird <agonzalez@codeforamerica.org>
2024-06-25 15:05:31 -0700

    Update Parish.java with new values from spreadsheet

--- src/main/java/org/ladocuploader/app/data/enums/Parish.java
+++ src/main/java/org/ladocuploader/app/data/enums/Parish.java
@@ -5,73 +5,75 @@ import lombok.Getter;
 
 @Getter
 public enum Parish {
-  
-  ACADIA("Acadia", "825 Kaliste Saloom Street", "","Lafayette", "70508"),
-  ALLEN("Allen", "116 SW Railroad., ", "Suite B", "Ville Platte", "70586"),
-  ASCENSION("Ascension", "1442 Tiger Drive", "","Thibodaux", "70301"),
-  ASSUMPTION("Assumption", "1442 Tiger Drive", "", "Thibodaux", "70301"),
-  AVOYELLES("Avoyelles", "900 Murray Street", "", "Alexandria", "71301"),
-  BEAUREGARD("Beauregard", "1417 Gadwall Street", "", "Lake Charles", "70615"),
-  BIENVILLE("Bienville", "1525 Fairfield Ave., ", "Suite 141", "Shreveport", "71101"),
-  BOSSIER("Bossier", "1525 Fairfield Ave., ", "Suite 141", "Shreveport", "71101"),
-  CADDO("Caddo", "1525 Fairfield Ave., ", "Suite 141", "Shreveport", "71101"),
-  CALCASIEU("Calcasieu", "1417 Gadwall Street", "", "Lake Charles", "70615"),
-  CALDWELL("Caldwell", "951 Century Blvd.", "", "Monroe", "71202"),
-  CAMERON("Cameron", "1417 Gadwall Street", "", "Lake Charles", "70615"),
-  CATAHOULA("Catahoula", "900 Murray Street", "", "Alexandria",  "71301"),
-  CLAIBORNE("Claiborne", "1525 Fairfield Ave., ", "Suite 141", "Shreveport", "71101"),
-  CONCORDIA("Concordia", "900 Murray Street", "", "Alexandria", "71301"),
-  DESOTO("DeSoto", "1774 Texas Street", "", "Natchitoches", "71457"),
-  EAST_BATON_ROUGE("East Baton Rouge", "5825 Florida Blvd.", "", "Baton Rouge","70806"),
-  EAST_CARROLL("East Carroll", "1614 Felicia Drive", "", "Tallulah",  "71282"),
-  EAST_FELICIANA("East Feliciana", "5825 Florida Blvd.", "", "Baton Rouge", "70806"),
-  EVANGELINE("Evangeline", "116 SW Railroad., ", "Suite B", "Ville Platte", "70586"),
-  FRANKLIN("Franklin", "1614 Felicia Drive", "", "Tallulah",  "71282"),
-  GRANT("Grant", "900 Murray Street", "", "Alexandria",  "71301"),
-  IBERIA("Iberia", "825 Kaliste Saloom Street", "", "Lafayette", "70508"),
-  IBERVILLE("Iberville", "5825 Florida Blvd.", "", "Baton Rouge", "70806"),
-  JACKSON("Jackson", "1525 Fairfield Ave., ","Suite 141", "Shreveport", "71101"),
-  JEFFERSON("Jefferson", "1630 Iberville St., ", "Suite 2800", "New Orleans", "70112"),
 
-  JEFFERSON_DAVIS("Jefferson Davis", "1417 Gadwall Street", "", "Lake Charles", "70615"),
-  LAFAYETTE("Lafayette", "825 Kaliste Saloom Street", "", "Lafayette",  "70508"),
-  LAFOURCHE("Lafourche", "1442 Tiger Drive", "", "Thibodaux", "70301"),
-  LASALLE("LaSalle", "900 Murray Street", "", "Alexandria", "71301"),
-  LINCOLN("Lincoln", "951 Century Blvd.", "", "Monroe",  "71202"),
-  LIVINGSTON("Livingston", "606 S. 1st Street", "", "Amite",  "70422"),
-  MADISON("Madison", "1614 Felicia Drive", "",  "Tallulah",  "71282"),
-  MOREHOUSE("Morehouse", "951 Century Blvd.", "", "Monroe","71202"),
-  NATCHITOCHES("Natchitoches", "1774 Texas Street", "", "Natchitoches", "71457"),
-  ORLEANS("Orleans", "1630 Iberville St., ", " Suite 2800", "New Orleans", "70112"),
-  OUACHITA("Ouachita", "951 Century Blvd.", "", "Monroe", "71202"),
-  PLAQUEMINES("Plaquemines", "1630 Iberville St., ", "Suite 2800", "New Orleans", "70112"),
-  POINTE_COUPEE("Pointe Coupee", "5825 Florida Blvd.", "", "Baton Rouge",  "70806"),
-  RAPIDES("Rapides", "900 Murray Street", "", "Alexandria", "71301"),
-  RED_RIVER("Red River", "1774 Texas Street", "", "Natchitoches",  "71457"),
-  RICHLAND("Richland", "1614 Felicia Drive", "", "Tallulah",  "71282"),
-  SABINE("Sabine", "1774 Texas Street", "", "Natchitoches", "71457"),
-  ST_BERNARD("St. Bernard", "1630 Iberville St, ", "Suite 2800", "New Orleans", "70112"),
-  ST_CHARLES("St. Charles", "1442 Tiger Drive", "", "Thibodaux", "70301"),
-  ST_HELENA("St. Helena", "606 S. 1st Street", "", "Amite", "70422"),
-  ST_JAMES("St. James", "1442 Tiger Drive", "", "Thibodaux", "70301"),
-  ST_JOHN("St. John", "1442 Tiger Drive", "", "Thibodaux", "70301"),
-  ST_LANDRY("St. Landry", "116 SW Railroad., ", "Suite B", "Ville Platte", "70586"),
-  ST_MARTIN("St. Martin", "825 Kaliste Saloom Street", "", "Lafayette", "0508"),
-  ST_MARY("St. Mary", "825 Kaliste Saloom Street", "", "Lafayette", "70508"),
-  ST_TAMMANY("St. Tammany", "606 S. 1st Street", "", "Amite", "70422"),
-  TANGIPAHOA("Tangipahoa", "606 S. 1st Street", "", "Amite", "0422"),
-  TENSAS("Tensas", "1614 Felicia Drive", "", "Tallulah", "71282"),
-  TERREBONNE("Terrebonne", "1442 Tiger Drive", "", "Thibodaux", "70301"),
-  UNION("Union", "951 Century Blvd.", "",  "Monroe", "71202"),
-  VERMILION("Vermilion", "825 Kaliste Saloom Street", "", "Lafayette", "70508"),
-  VERNON("Vernon", "900 Murray Street", "", "Alexandria", "71301"),
-  WASHINGTON("Washington", "606 S. 1st Street", "", "Amite", "70422"),
-  WEBSTER("Webster", "1525 Fairfield Ave., ", "Suite 141", "Shreveport", "71101"),
-  WEST_BATON_ROUGE("West Baton Rouge", "5825 Florida Blvd.", "", "Baton Rouge", "70806"),
-  WEST_CARROLL("West Carroll", "1614 Felicia Drive", "", "Tallulah", "71282"),
-  WEST_FELICIANA("West Feliciana", "5825 Florida Blvd.", "", "Baton Rouge", "70806"),
-  WINN("Winn", "900 Murray Street", "", "Alexandria", "71301");
+  ACADIA("Acadia", "Acadia", "PO Box 649", "", "Crowley", "70527", "600 N Avenue G", "", "Crowley", "70526"),
+  ALLEN("Allen", "Allen", "PO Box 280", "", "Oberlin", "70655", "213A N 1st St", "", "Oberlin", "70655"),
+  ASCENSION("Ascension", "Ascension", "1078 E Worthy St", "", "Gonzales", "70737", "1078 E Worthy St", "", "Gonzales", "70737"),
+  ASSUMPTION("Assumption", "Ascension", "1078 E Worthy St", "", "Gonzales", "70737", "1078 E Worthy St", "", "Gonzales", "70737"),
+  AVOYELLES("Avoyelles", "Avoyelles", "607 Tunica Dr W", "", "Marksville", "71351", "607 Tunica Dr W", "", "Marksville", "71351"),
+  BEAUREGARD("Beauregard", "Beauregard", "PO Box 848", "", "Deridder", "70634", "1891 Highway 190 W", "", "Deridder", "70634"),
+  BIENVILLE("Bienville", "Lincoln", "811 N Service Rd E", "", "Ruston", "71270", "811 N Service Rd E", "", "Ruston", "71270"),
+  BOSSIER("Bossier", "Caddo", "1525 Fairfield Ave", "Ste 220", "Shreveport", "71101", "1525 Fairfield Ave", "Ste 220", "Shreveport", "71101"),
+  CADDO("Caddo", "Caddo", "1525 Fairfield Ave", "Ste 220", "Shreveport", "71101", "1525 Fairfield Ave", "Ste 220", "Shreveport", "71101"),
+  CALCASIEU("Calcasieu", "Calcasieu", "650 Petro Point Dr", "", "Lake Charles", "70607", "650 Petro Point Dr", "", "Lake Charles", "70607"),
+  CALDWELL("Caldwell", "Ouachita", "24 Accent Dr", "Ste 106", "Monroe", "71202", "24 Accent Dr", "Ste 106", "Monroe", "71202"),
+  CAMERON("Cameron", "Calcasieu", "650 Petro Point Dr", "", "Lake Charles", "70607", "650 Petro Point Dr", "", "Lake Charles", "70607"),
+  CATAHOULA("Catahoula", "Catahoula", "124 Airport Rd", "", "Jonesville", "71343", "124 Airport Rd", "", "Jonesville", "71343"),
+  CLAIBORNE("Claiborne", "Claiborne", "622 E 2nd St", "", "Homer", "71040", "622 E 2nd St", "", "Homer", "71040"),
+  CONCORDIA("Concordia", "Catahoula", "124 Airport Rd", "", "Jonesville", "71343", "124 Airport Rd", "", "Jonesville", "71343"),
+  DESOTO("DeSoto", "DeSoto", "104 Crosby St", "", "Mansfield", "71052", "104 Crosby St", "", "Mansfield", "71052"),
+  EAST_BATON_ROUGE("East Baton Rouge", "East Baton Rouge", "1919 North Blvd", "", "Baton Rouge", "70806", "1919 North Blvd", "", "Baton Rouge", "70806"),
+  EAST_CARROLL("East Carroll", "West Carroll", "702 E Jefferson St", "", "Oak Grove", "71263", "702 E Jefferson St", "", "Oak Grove", "71263"),
+  EAST_FELICIANA("East Feliciana", "East Feliciana", "PO Box 926", "", "Clinton", "70722", "12476 Feliciana Dr", "", "Clinton", "70722"),
+  EVANGELINE("Evangeline", "St. Landry", "6069 I 49 S Service Rd", "Ste A", "Opelousas", "70570", "6069 I 49 S Service Rd", "Ste A", "Opelousas", "70570"),
+  FRANKLIN("Franklin", "Franklin", "2406 West St", "", "Winnsboro", "71295", "2406 West St", "", "Winnsboro", "71295"),
+  GRANT("Grant", "Rapides", "PO Box 8557", "", "Alexandria", "71306", "900 Murray St", "Ste K257", "Alexandria", "71301"),
+  IBERIA("Iberia", "Iberia", "2500 Highway 14", "", "New Iberia", "70560", "2500 Highway 14", "", "New Iberia", "70560"),
+  IBERVILLE("Iberville", "Iberville", "PO Box 778", "", "Plaquemine", "70765", "23075 Highway 1", "", "Plaquemine", "70764"),
+  JACKSON("Jackson", "Lincoln", "811 N Service Rd E", "", "Ruston", "71270", "811 N Service Rd E", "", "Ruston", "71270"),
+  JEFFERSON("Jefferson", "Jefferson - West Bank", "2150 Westbank Expy", "Ste 201", "Harvey", "70058", "2150 Westbank Expy", "Ste 201", "Harvey", "70058"),
+  JEFFERSON_DAVIS("Jefferson Davis", "Calcasieu", "650 Petro Point Dr", "", "Lake Charles", "70607", "650 Petro Point Dr", "", "Lake Charles", "70607"),
+  LAFAYETTE("Lafayette", "Lafayette", "825 Kaliste Saloom Rd", "# 100", "Lafayette", "70508", "825 Kaliste Saloom Rd", "Bldg 6 Ste 100", "Lafayette", "70508"),
+  LAFOURCHE("LaFourche", "Terrebonne", "1012 W Tunnel Blvd", "", "Houma", "70360", "1012 W Tunnel Blvd", "", "Houma", "70360"),
+  LASALLE("LaSalle", "Catahoula", "124 Airport Rd", "", "Jonesville", "71343", "124 Airport Rd", "", "Jonesville", "71343"),
+  LINCOLN("Lincoln", "Lincoln", "811 N Service Rd E", "", "Ruston", "71270", "811 N Service Rd E", "", "Ruston", "71270"),
+  LIVINGSTON("Livingston", "Livingston", "28446 Charley Watts Road", "", "Livingston", "70754", "28446 Charley Watts Rd", "", "Livingston", "70754"),
+  MADISON("Madison", "Madison", "1707 Felicia Ave", "", "Tallulah", "71282", "1707 Felicia Ave", "", "Tallulah", "71282"),
+  MOREHOUSE("Morehouse", "Morehouse", "1045 E Madison Ave", "", "Bastrop", "71220", "1045 E Madison Ave", "", "Bastrop", "71220"),
+  NATCHITOCHES("Natchitoches", "Natchitoches", "106 Charlene St", "", "Natchitoches", "71457", "106 Charlene St", "", "Natchitoches", "71457"),
+  ORLEANS("Orleans", "Algiers", "DCFS Family Support/Economic Stability P O Box 260031", "", "Baton Rouge", "70826", "2703 General DeGaulle Drive", "", "New Orleans", "70114"),
+  OUACHITA("Ouachita", "Ouachita", "24 Accent Dr", "Ste 106", "Monroe", "71202", "24 Accent Dr", "Ste 106", "Monroe", "71202"),
+  PLAQUEMINES("Plaquemines", "Jefferson - West Bank", "2150 Westbank Expy", "Ste 201", "Harvey", "70058", "2150 Westbank Expy", "Ste 201", "Harvey", "70058"),
+  POINTE_COUPLEE("Pointe Coupee", "East Baton Rouge", "1919 North Blvd", "", "Baton Rouge", "70806", "1919 North Blvd", "", "Baton Rouge", "70806"),
+  RAPIDES("Rapides", "Rapides", "PO Box 8557", "", "Alexandria", "71306", "900 Murray St", "Ste K257", "Alexandria", "71301"),
+  RED_RIVER("Red River", "Red River", "5040 Cut Off Rd", "", "Coushatta", "71019", "5040 Cut Off Rd", "", "Coushatta", "71019"),
+  RICHLAND("Richland", "Ouachita", "24 Accent Dr", "Ste 106", "Monroe", "71202", "24 Accent Dr", "Ste 106", "Monroe", "71202"),
+  SABINE("Sabine", "Natchitoches", "106 Charlene St", "", "Natchitoches", "71457", "106 Charlene St", "", "Natchitoches", "71457"),
+  ST_BERNARD_ALGIERS("St. Bernard", "Algiers", "DCFS Family Support/Economic Stability P O Box 260031", "", "Baton Rouge", "70826", "2703 General DeGaulle Drive", "", "New Orleans", "70114"),
+  ST_CHARLES("St. Charles", "St. John", "425 W Airline Hwy", "Suite C", "LaPlace", "70068", "425 W Airline Hwy", "Suite C", "LaPlace", "70068"),
+  ST_HELENA("St. Helena", "Tangipahoa", "1211 NW Central Avenue", "", "Amite", "70422", "1211 NW Central Ave", "", "Amite", "70422"),
+  ST_JAMES("St. James", "St. John", "425 W Airline Hwy", "Suite C", "LaPlace", "70068", "425 W Airline Hwy", "Suite C", "LaPlace", "70068"),
+  ST_JOHN("St. John", "St. John", "425 W Airline Hwy", "Suite C", "LaPlace", "70068", "425 W Airline Hwy", "Suite C", "LaPlace", "70068"),
+  ST_LANDRY("St. Landry", "St. Landry", "6069 I 49 S Service Rd", "Ste A", "Opelousas", "70570", "6069 I 49 S Service Rd", "Ste A", "Opelousas", "70570"),
+  ST_MARTIN("St. Martin", "Lafayette", "825 Kaliste Saloom Rd", "# 100", "Lafayette", "70508", "825 Kaliste Saloom Rd", "Bldg 6 Ste 100", "Lafayette", "70508"),
+  ST_MARY("St. Mary", "Iberia", "2500 Highway 14", "", "New Iberia", "70560", "2500 Highway 14", "", "New Iberia", "70560"),
+  ST_TAMMANY("St. Tammany", "St. Tammany", "351 Holiday Blvd", "", "Covington", "70433", "351 Holiday Blvd", "", "Covington", "70433"),
+  TANGIPAHOA("Tangipahoa", "Tangipahoa", "1211 NW Central Avenue", "", "Amite", "70422", "1211 NW Central Ave", "", "Amite", "70422"),
+  TENSAS("Tensas", "Madison", "1707 Felicia Ave", "", "Tallulah", "71282", "1707 Felicia Ave", "", "Tallulah", "71282"),
+  TERREBONNE("Terrebonne", "Terrebonne", "1012 W Tunnel Blvd", "", "Houma", "70360", "1012 W Tunnel Blvd", "", "Houma", "70360"),
+  UNION("Union", "Ouachita", "24 Accent Dr", "Ste 106", "Monroe", "71202", "24 Accent Dr", "Ste 106", "Monroe", "71202"),
+  VERMILION("Vermilion", "Acadia", "PO Box 649", "", "Crowley", "70527", "600 N Avenue G", "", "Crowley", "70526"),
+  VERNON("Vernon", "Vernon", "PO Box 190", "", "New Llano", "71461", "1014 S 6th St", "", "Leesville", "71446"),
+  WASHINGTON("Washington", "Washington", "1017 Ontario Ave", "", "Bogalusa", "70427", "1017 Ontario Ave", "", "Bogalusa", "70427"),
+  WEBSTER("Webster", "Webster", "223 Pine St", "", "Minden", "71055", "223 Pine St", "", "Minden", "71055"),
+  WEST_BATON_ROUGE("West Baton Rouge", "East Baton Rouge", "1919 North Blvd", "", "Baton Rouge", "70806", "1919 North Blvd", "", "Baton Rouge", "70806"),
+  WEST_CARROLL("West Carroll", "West Carroll", "702 E Jefferson St", "", "Oak Grove", "71263", "702 E Jefferson St", "", "Oak Grove", "71263"),
+  WEST_FELICIANA("West Feliciana", "East Feliciana", "PO Box 926", "", "Clinton", "70722", "12476 Feliciana Dr", "", "Clinton", "70722"),
+  WINN("Winn", "Winn", "PO Box 231", "", "Winnfield", "71483", "1408 E Lafayette St", "", "Winnfield", "71483");
+  
   private final String displayName;
+  
+  private final String office;
 
   private final String mailingAddress1;
 
@@ -82,17 +84,35 @@ public enum Parish {
   private String mailingAddressState = "LA";
 
   private final String mailingAddressZipcode;
+  
+  private final String physicalAddress1;
+  
+  private final String physicalAddress2;
+  
+  private final String physicalAddressCity;
+  
+  private String physicalAddressState = "LA";
+    
+  private final String physicalAddressZipcode;
+  
+    
 
 
 
-  Parish(String displayName, String mailingAddress1, String mailingAddress2, String mailingAddressCity,
-         String mailingAddressZipcode) {
+  Parish(String displayName, String office, String mailingAddress1, String mailingAddress2, String mailingAddressCity,
+         String mailingAddressZipcode, String physicalAddress1, String physicalAddress2, String physicalAddressCity, String physicalAddressZipcode) {
     this.displayName = displayName;
+    this.office = office;
     this.mailingAddressCity = mailingAddressCity;
     this.mailingAddressState = "LA";
     this.mailingAddressZipcode = mailingAddressZipcode;
     this.mailingAddress1 = mailingAddress1;
     this.mailingAddress2 = mailingAddress2;
+    this.physicalAddress1 = physicalAddress1;
+    this.physicalAddress2 = physicalAddress2;
+    this.physicalAddressCity = physicalAddressCity;
+    this.physicalAddressState = "LA";
+    this.physicalAddressZipcode = physicalAddressZipcode;
   }
 
   public String getMailingAddressLine1() {
